65ef6cf4d6b183d0a298dc0df356600ba929e33b
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-07-31 01:32:04 +0200

    Started using bootstrap. Need feedback to continue the work
      - Fix missing $self in WebIrc::archive() accessor
      - Add oauth_connect_url() helper
      - Drafted welcome page text and styling
      - Remove login page. No need for an extra page if we're doing Oauth2
      - Add Facebook connect button to register page
      - Load Facebook javascript and css to style .fb-login-button

--- lib/WebIrc.pm
+++ lib/WebIrc.pm
@@ -6,23 +6,35 @@ use Mojo::Redis;
 has 'connection' => sub { Mojo::Redis->new(server=>'127.0.0.1:6379') };
 has 'core'       => sub { WebIrc::Core->new(redis=>shift->connection)};
 has 'archive'    => sub {
+  my $self = shift;
   WebIrc::Core::Archive->new(  shift->config->{archive} ||
   $self->path_to('archive')) };
 
 # This method will run once at server start
 sub startup {
   my $self = shift;
+  my $config = $self->plugin('Config');
 
-  $self->plugin('OAuth2');
+  $self->plugin(OAuth2 => $config->{'OAuth2'});
 
-  $self->defaults(layout => 'default');
+  $self->helper(oauth_connect_url => sub {
+    my($c, $use_current) = @_;
+    $c->get_authorize_url(facebook => (
+      $use_current ? () : (redirect_uri => $c->url_for('/account')->to_abs->to_string),
+      scope => $config->{'OAuth2'}{'facebook'}{'scope'},
+    ));
+  });
+
+  $self->defaults(
+    layout => 'default',
+    logged_in => 0,
+  );
+
   # Router
   my $r = $self->routes;
 
   # Normal route to controller
   $r->get('/')->to(template => 'index');
-  $r->get('/login')->to(template => 'user/login');
-  $r->post('/login')->to('user#login');
   $r->get('/register')->to(template => 'user/register');
   $r->post('/register')->to('user#register');
   my $c=$r->bridge('/c')->to('user#auth');
@@ -32,7 +44,10 @@ sub startup {
   $c->route('/archive/search')->to('archive#search');
   $c->route('/archive/:server/:channel')->to('archive#view');
 
-  $self->core->start;
+  # add NO_REDIS since batman is just going to do bootstrap now,
+  # and Mojo::Redis seem to eat 100% cpu when the backend server
+  # is not there...
+  $self->core->start unless $ENV{'NO_REDIS'};
 }
 
 1;
---
+++ public/js/wirc.js
@@ -0,0 +1,7 @@
+// want to lazy load javascripts to prevent loading indicator
+(function(d){
+  var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+  js = d.createElement('script'); js.id = id; js.async = true;
+  js.src = "//connect.facebook.net/en_US/all.js";
+  d.getElementsByTagName('head')[0].appendChild(js);
+}(document));
