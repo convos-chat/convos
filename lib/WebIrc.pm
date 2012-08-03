6a169b4583bb2b1d3221f5ab86b9fe410db73e20
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-08-03 15:24:19 +0200

    Add page_header() helper and pod

--- lib/WebIrc.pm
+++ lib/WebIrc.pm
@@ -1,30 +1,57 @@
 package WebIrc;
+
+=head1 NAME
+
+WebIrc - IRC client on web
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
 use Mojo::Base 'Mojolicious';
 use WebIrc::Core;
 use Mojo::Redis;
 
+=head1 ATTRIBUTES
+
+=head2 connection
+
+Holds a L<Mojo::Redis> object.
+
+=head2 core
+
+Holds a L<WebIrc::Core> object.
+
+=head2 archive
+
+=cut
+
 has 'connection' => sub { Mojo::Redis->new(server=>'127.0.0.1:6379') };
 has 'core'       => sub { WebIrc::Core->new(redis=>shift->connection)};
+
+# is this formatting done by a human or tidy..?
 has 'archive'    => sub {
   my $self = shift;
   WebIrc::Core::Archive->new(  shift->config->{archive} ||
   $self->path_to('archive')) };
 
-# This method will run once at server start
+=head1 METHODS
+
+=head2 startup
+
+This method will run once at server start
+
+=cut
+
 sub startup {
   my $self = shift;
   my $config = $self->plugin('Config');
 
   $self->plugin(OAuth2 => $config->{'OAuth2'});
 
-  $self->helper(oauth_connect_url => sub {
-    my($c, $use_current) = @_;
-    $c->get_authorize_url(facebook => (
-      $use_current ? () : (redirect_uri => $c->url_for('/account')->to_abs->to_string),
-      scope => $config->{'OAuth2'}{'facebook'}{'scope'},
-    ));
-  });
-
+  $self->add_helpers($config);
   $self->defaults(
     layout => 'default',
     logged_in => 0,
@@ -49,5 +76,54 @@ sub startup {
   # is not there...
   $self->core->start unless $ENV{'NO_REDIS'};
 }
+
+=head2 add_helpers
+
+Will add thease helpers:
+
+=over 4
+
+=item oauth_connect_url
+
+This is a modification of L<Mojolicious::Plugin::OAuth2/get_authorize_url>
+since Jan Henning forgot to pass on the correct arguments.
+
+=item page_header
+
+Used to set/retrieve the page header used by C<layout/default.html.ep>
+
+=back
+
+=cut
+
+sub add_helpers {
+  my($self, $config) = @_;
+
+  $self->helper(oauth_connect_url => sub {
+    my($c, $use_current) = @_;
+    $c->get_authorize_url(facebook => (
+      $use_current ? () : (redirect_uri => $c->url_for('/account')->to_abs->to_string),
+      scope => $config->{'OAuth2'}{'facebook'}{'scope'},
+    ));
+  });
+
+  $self->helper(page_header => sub {
+    $_[0]->stash('page_header', $_[1]) if @_ == 2;
+    $_[0]->stash('title', Mojo::DOM->new($_[1])->all_text) if @_ == 2 and not $self->stash('title');
+    $_[0]->stash('page_header') // 'Wirc';
+  });
+}
+
+=head1 COPYRIGHT
+
+Nordaaker
+
+=head1 AUTHOR
+
+Jan Henning Thorsen - jhthorsen@cpan.org
+
+Marcus Ramberg
+
+=cut
 
 1;
--- lib/WebIrc/Client.pm
+++ lib/WebIrc/Client.pm
@@ -1,11 +1,31 @@
 package WebIrc::Client;
 
+=head1 NAME
+
+WebIrc::Client
+
+=cut
+
 use Mojo::Base 'Mojolicious::Controller';
+
+=head1 METHODS
+
+=head2 view
+
+=cut
 
 sub view {
   my $self = shift;
 
-  $self->stash(is_channel => $self->param('target') =~ /^#/ ? 1 : 0);
+  $self->stash(logged_in => 1); # TODO: Remove this once login logic is written
 }
+
+=head1 AUTHOR
+
+Jan Henning Thorsen
+
+Marcus Ramberg
+
+=cut
 
 1;
