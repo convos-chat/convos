af4bd5e9c8ba18575267075e8d25e5057b76aa3a
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-08-14 22:29:33 +0200

    Reformatted and refactored javascript
      - Add Simple JavaScript Templating
        From http://ejohn.org/blog/javascript-micro-templating/
        John Resig - http://ejohn.org/ - MIT Licensed
      - Add structure.js
        https://github.com/jimmycuadra/structure/commit/ec4bf9b55c7a3498c2ea9f97451b0e4386f0d6ae

--- public/js/wirc.js
+++ public/js/wirc.js
@@ -1,110 +1,132 @@
-var BASEURL = window.location.href;
-
 (function($) {
-  var commands = {
+
+Structure.registerModule('Wirc', {
+  websocket: function(path, callbacks) {
+    var url = BASEURL.replace(/^http/, 'ws') + path;
+    var websocket = new WebSocket(BASEURL.replace(/^http/, 'ws') + '/socket');
+    window.console && console.log('[websocket] ' + url);
+    $.each(callbacks, function(name, callback) { websocket[name] = callback });
+    return websocket;
+  }
+}); // End Wirc
+
+Structure.registerModule('Wirc.Chat', {
+  autocomplete_commands: {
     '/join': { append: ' #' },
     '/msg': { append: ' ' },
     '/me': { append: ' ' },
     '/nick': { append: ' ' },
     '/part': {}
-  };
+  },
+  print: function(data) {
+    var $previous = this.$messages.find('li .sender').eq(-1).parent();
+    var next;
 
-  $(document).ready(function() {
-    BASEURL = $('script[src$="jquery.js"]').get(0).src.replace(/\/js\/[^\/]+$/, '');
+    if(data.error) {
+      next = tmpl('tmpl_li_error', data);
+    }
+    else if($previous.find('.sender a').text() == data.sender) {
+      next = tmpl('tmpl_li_message', { message: data.message, timestamp: data.timestamp });
+    }
+    else {
+      next = tmpl('tmpl_li_message', data);
+    }
 
-    window.console && console.log('Connecting to ' + BASEURL.replace(/^http/, 'ws') + '/socket ...');
-    var server = unescape(window.location.href.split('/')[4] || '');
-    var target = unescape(window.location.href.split('/')[5] || '');
-    var $messages = $('#messages');
-    var chat = new WebSocket(BASEURL.replace(/^http/, 'ws') + '/socket');
-    var ui = {
-      $input: $('#message input[type="text"]'),
-      history_index: 0,
-      history: []
+    this.$messages.append(next);
+    this.scrollToBottom();
+  },
+  scrollToBottom: function() {
+    this.$messages.scrollTop(this.$messages.get(0).scrollHeight - this.$messages.height());
+  },
+  receiveData: function(e) {
+    var data = $.parseJSON(e.data);
+
+    window.console && console.log('[websocket] > ' + e.data);
+
+    if(data.error || data.message) {
+      this.print(data);
+    }
+  },
+  sendData: function(data) {
+    // TODO: Figure out if JSON.stringify() works in other browsers than chrome
+    try {
+      this.websocket.send(JSON.stringify(data));
+      window.console && console.log('[websocket] < ' + JSON.stringify(data));
+    } catch(e) {
+      window.console && console.log('[websocket] ! ' + e);
+      this.print({ error: e });
     };
-
-    $messages.scrollTop($messages.get(0).scrollHeight - $messages.height());
-
-    chat.onmessage = function(e) {
-      var data = typeof e.data == 'object' ? e.data : $.parseJSON(e.data);
-      var $previous = $messages.find('li .sender').eq(-1).parent();
-      var $next = $('<li></li>');
-
-      // TODO: Need to populate the other channels if data.params[0]
-      // is not the current channel/user? This can be useful to add (n)
-      // unread messages as status to the channel/user list
-
-      if(data.error) {
-        $next.append('<i class="icon-exclamation-sign"></i> Error: ' + data.error);
+  },
+  connectToWebSocket: function() {
+    var self = this;
+    self.websocket = Wirc.websocket('/socket', {
+      onmessage: self.receiveData,
+      onerror: function(e) {
+        self.print({ error: e.data });
+        self.$input.attr('disabled', 'disabled');
+      },
+      onclose: function() {
+        self.print({ error: 'Disconnected.' });
+        self.$input.attr('disabled', 'disabled');
       }
-      else if(data.joined) {
-        console.log(data);
-      }
-      else if($previous.find('.sender a').text() == data.sender) {
-        $next.append('<span class="message">' + data.message + '</span>');
-      }
-      else {
-        var $sender = $previous.find('.sender').clone();
-        $sender.find('a').attr({ href: BASEURL + '/chat/' + data.sender }).text(data.sender);
-        $next.append($sender);
-        $next.append('<span class="timestamp">' + data.timestamp + '</span>');
-        $next.append('<span class="message" title="' + data.timestamp + '">' + data.message + '</span>');
-      }
-
-      $messages.append($next);
-      $messages.scrollTop($messages.get(0).scrollHeight - $messages.height());
-    };
-    chat.end = function() {
-        ui.print({ sender: '&client', message: 'Disconnected. <a href="">Reconnect?</a>' });
-        ui.$input.attr('disabled', 'disabled');
-    };
-
-    ui.$input.attr('autocomplete', 'off');
-    ui.$input.parents('form').submit(function() {
-      try {
-        // TODO: Figure out if JSON.stringify() works in other browsers than chrome
-        chat.send(JSON.stringify({ command: ui.$input.val(), target: target }));
-        window.console && console.log('send: ' + ui.$input.val());
-      } catch(e) {
-        window.console && console.log('Could not send to websocket: ' + e);
-        chat.onmessage({ data: { error: e } });
-      };
-      ui.$input.val('');
-      return false;
     });
+  },
+  setupUI: function() {
+    var self = this;
 
-    ui.$input.typeahead({
-      source: Object.keys(commands),
+    window.console && console.log('[Wirc.Chat.setupUI] input.autocomplete');
+    self
+    .$input
+    .attr('autocomplete', 'off')
+    .focus()
+    .typeahead({
+      source: Object.keys(self.autocomplete_commands),
       items: 5,
       matcher: function(item) {
         return item.toLowerCase().indexOf(this.query.toLowerCase()) == 0;
       },
       updater: function(item) {
-        if(commands[item].append) {
-          return item + commands[item].append;
+        if(self.autocomplete_commands[item].append) {
+          return item + self.autocomplete_commands[item].append;
         }
         else {
-          ui.$input.val(item);
-          ui.$input.parents('form').submit();
+          self.$input.val(item);
+          self.$input.parents('form').submit();
           return '';
         }
       }
     });
+
+    window.console && console.log('[Wirc.Chat.setupUI] form.submit');
+    self.$input.parents('form').submit(function() {
+      self.sendData({ command: self.$input.val(), target: self.target });
+      self.$input.val('');
+      return false;
+    });
+
+    self.scrollToBottom();
+  },
+  start: function($) {
+    this.server = unescape(window.location.href.split('/')[4] || '');
+    this.target = unescape(window.location.href.split('/')[5] || '');
+    this.$messages = $('#messages');
+    this.$input = $('#message input[type="text"]');
+    this.connectToWebSocket();
+    this.setupUI();
+    window.console && console.log('[Wirc.Chat.start] ', this);
+  }
+}); // End Wirc.Chat
+
+  jQuery(document).ready(function() {
+    BASEURL = $('script[src$="jquery.js"]').get(0).src.replace(/\/js\/[^\/]+$/, '');
+    if($('#messages')) Wirc.Chat.start($);
   });
 })(jQuery);
 
-/*
-// want to lazy load javascripts to prevent loading indicator
-(function(d){
-  var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
-  js = d.createElement('script'); js.id = id; js.async = true;
-  js.src = "//connect.facebook.net/en_US/all.js";
-  d.getElementsByTagName('head')[0].appendChild(js);
-}(document));
-*/
+BASEURL = window.location.href;
 
 /*
- * flash fallback
+ * Flash fallback for websocket
  *
 if(!('WebSocket' in window)) {
     document.write([
