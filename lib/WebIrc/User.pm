43335f03d743551e97eca452e10c8beefddcd2d7
Marcus Ramberg <marcus@nordaaker.com>
2012-08-15 00:04:48 +0200

    Basic login works

--- lib/WebIrc/User.pm
+++ lib/WebIrc/User.pm
@@ -10,15 +10,41 @@ use Mojo::Base 'Mojolicious::Controller';
 
 =head1 METHODS
 
+=head2 auth
+
+Check authentication and login
+
+=cut
+
+sub auth {
+  my $self=shift;
+  return 1 if $self->session('uid');
+  $self->redirect_to('login');
+  return 0;
+}
+
 =head2 login
 
-What should this do? Aren't we using oauth2? or should we also support
-a Wirc account database?
+Authenticate local user
 
 =cut
 
 sub login {
   my $self=shift;
+  $self->redis->get('user:'.$self->param('login').':uid',$self->parallol(sub {
+    my ($redis,$uid)=@_;
+    warn("uid $uid");
+    return $self->stash( message=>"Invalid username/password" ) unless  $uid && $uid =~ /\d+/;
+    $redis->get('user:'.$self->param('login').':digest', $self->parallol(sub {
+      my ($redis,$digest)=@_;
+      warn "Testing $digest";
+      if(crypt($self->param('password'),$digest) eq $digest) {
+        $self->session('uid' => $uid);
+        $self->redirect_to('/setup');
+      }
+      else { $self->stash(message=>'Invalid username/password.'); }
+    }));
+  }));
 }
 
 =head2 register
@@ -29,6 +55,34 @@ See L</login>.
 
 sub register {
   my $self=shift;
+  $self->render_later;
+  if($self->param('login') =~ m/^[\w]{4,15}$/) {
+    $self->redis->get('user:'.$self->param('login').':uid',$self->parallol(sub {
+      my ($redis,$uid)=@_;
+      unless($uid) {
+        if($self->param('password') && length($self->param('password'))>5){
+          $redis->incr('user:uids',$self->parallol(sub {
+            my ($redis,$uid)=@_;
+            $redis->set('user:'.$self->param('login').':uid',$uid);
+            $redis->set('user:'.$self->param('login').':digest',
+              crypt($self->param('password'),
+                join('', ('.', '/', 0..9, 'A'..'Z', 'a'..'z')[rand 64, rand 64])));
+            $self->session(uid=>$uid);
+            $self->redirect_to('/setup');
+          }));
+        }
+        else {
+          $self->stash('message'=>'The password must be at least 6 characters long');
+        }
+      }
+      else {
+        $self->stash(message=>'This username is taken');
+      }
+    }));
+  }
+  else {
+    $self->stash(message=>'Username must consist of letters and numbers and be 4-15 characters long');
+  }
 }
 
 =head1 COPYRIGHT
