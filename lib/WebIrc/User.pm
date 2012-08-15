9d2582703e25bb4429e111e8a061acbc627dbafc
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-08-15 16:06:02 +0200

    Reversed logic in register()
      Doing if(...) { ...; return } prevents too much indentation in the code.
      Also, this change include testing the password -before- doing redis
      actions.

--- lib/WebIrc/User.pm
+++ lib/WebIrc/User.pm
@@ -56,35 +56,36 @@ See L</login>.
 
 sub register {
   my $self=shift;
-  $self->render_later;
+
   if($self->param('login') =~ m/^[\w]{4,15}$/) {
-    $self->redis->get('user:'.$self->param('login').':uid',$self->parallol(sub {
-      my ($redis,$uid)=@_;
-      unless($uid) {
-        if($self->param('password') && length($self->param('password'))>5){
-          $redis->incr('user:uids',$self->parallol(sub {
-            my ($redis,$uid)=@_;
-            $redis->set('user:'.$self->param('login').':uid',$uid);
-            $redis->set('user:'.$self->param('login').':digest',
-              crypt($self->param('password'),
-                join('', ('.', '/', 0..9, 'A'..'Z', 'a'..'z')[rand 64, rand 64])));
-            $self->session(uid=>$uid);
-            $self->session('login' => $self->param('login'));
-            $self->redirect_to('/setup');
-          }));
-        }
-        else {
-          $self->stash('message'=>'The password must be at least 6 characters long');
-        }
-      }
-      else {
-        $self->stash(message=>'This username is taken');
-      }
-    }));
-  }
-  else {
     $self->stash(message=>'Username must consist of letters and numbers and be 4-15 characters long');
+    return;
   }
+  if($self->param('password') and 5 < length $self->param('password')){
+    $self->stash('message'=>'The password must be at least 6 characters long');
+    return;
+  }
+
+  $self->render_later;
+  $self->redis->get('user:'.$self->param('login').':uid',$self->parallol(sub {
+    my ($redis,$uid)=@_;
+
+    if($uid) {
+      $self->stash(message=>'This username is taken');
+      return;
+    }
+
+    $redis->incr('user:uids',$self->parallol(sub {
+      my ($redis,$uid)=@_;
+      $redis->set('user:'.$self->param('login').':uid',$uid);
+      $redis->set('user:'.$self->param('login').':digest',
+        crypt($self->param('password'),
+          join('', ('.', '/', 0..9, 'A'..'Z', 'a'..'z')[rand 64, rand 64])));
+      $self->session(uid=>$uid);
+      $self->session(login => $self->param('login'));
+      $self->redirect_to('/setup');
+    }));
+  }));
 }
 
 =head1 COPYRIGHT
