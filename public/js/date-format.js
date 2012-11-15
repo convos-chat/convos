6de6edc50fb28d24fab1651444d58c98feec7e43
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-11-15 21:13:24 +0100

    Fix data-format.js with jslint

--- public/js/date-format.js
+++ public/js/date-format.js
@@ -2,14 +2,14 @@
   var days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
   var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
   var markers = {
-    d: [ 'getDate', function(v) { return ("0" + v).substr(-2, 2) } ],
-    m: [ 'getMonth', function(v) { return ("0" + v).substr(-2, 2) } ],
-    n: [ 'getMonth', function(v) { return months[v] } ],
-    w: [ 'getDay', function(v) { return days[v] } ],
+    d: [ 'getDate', function(v) { return ("0" + v).substr(-2, 2); } ],
+    m: [ 'getMonth', function(v) { return ("0" + v).substr(-2, 2); } ],
+    n: [ 'getMonth', function(v) { return months[v]; } ],
+    w: [ 'getDay', function(v) { return days[v]; } ],
     y: [ 'getFullYear'],
-    H: [ 'getHours', function(v) { return ("0" + v).substr(-2, 2) } ],
-    M: [ 'getMinutes', function(v) { return ("0" + v).substr(-2, 2) } ],
-    S: [ 'getSeconds', function(v) { return ("0" + v).substr(-2, 2) } ],
+    H: [ 'getHours', function(v) { return ("0" + v).substr(-2, 2); } ],
+    M: [ 'getMinutes', function(v) { return ("0" + v).substr(-2, 2); } ],
+    S: [ 'getSeconds', function(v) { return ("0" + v).substr(-2, 2); } ],
     i: [ 'toISOString', null ]
   };
 
@@ -17,9 +17,9 @@
   Date.prototype.sprintf = function(format) {
     var d = this;
     return format.replace(/%(.)/g, function(m, p) {
-      var rv = d[ (markers[p])[0] ]()
-      if(markers[p][1] != null ) rv = markers[p][1](rv)
-      return rv
+      var rv = d[ (markers[p])[0] ]();
+      if(markers[p][1] !== null ) rv = markers[p][1](rv);
+      return rv;
     });
   };
 })();
