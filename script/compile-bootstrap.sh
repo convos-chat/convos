a332d54194de42d68911c1745f0730e0bfdd14ba
Marcus Ramberg <marcus@nordaaker.com>
2012-12-03 20:57:03 +0100

    Set up for overriding responsive-navbar, requires lessc changes

--- script/compile-bootstrap.sh
+++ script/compile-bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-lessc --strict-imports --include-path="vendor/bootstrap/less:vendor/font-awesome/less:templates/less" templates/less/custom-bootstrap.less > public/css/wirc.css
+lessc --include-path="templates/less:vendor/bootstrap/less:vendor/font-awesome/less" templates/less/custom-bootstrap.less > public/css/wirc.css
 cp -r vendor/font-awesome/font public/
