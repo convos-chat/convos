32191f29cbfcdb063627931851fec198e84fb68a
Marcus Ramberg <marcus@nordaaker.com>
2012-12-09 14:04:29 +0100

    Bundle modified lessc

--- script/compile-bootstrap.sh
+++ script/compile-bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-lessc --include-path="assets/less:vendor/bootstrap/less:vendor/font-awesome/less" assets/less/custom-bootstrap.less > public/css/wirc.css
+./vendor/less.js/bin/lessc --include-path="assets/less:vendor/bootstrap/less:vendor/font-awesome/less" assets/less/custom-bootstrap.less > public/css/wirc.css
 cp -r vendor/font-awesome/font public/
