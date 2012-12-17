7d102f186408fc266107f350bf3444855a8c4344
Marcus Ramberg <marcus@nordaaker.com>
2012-12-17 10:40:48 +0100

    Fix route to allow '.' in name

--- lib/WebIrc.pm
+++ lib/WebIrc.pm
@@ -143,7 +143,7 @@ sub startup {
   $private_r->get('/history/:page')->to('client#history');
   $private_r->websocket('/socket')->to('client#socket');
 
-  $private_r->get('/:cid/:target')->to('client#view')->name('channel.view');
+  $private_r->get('/:cid/*target')->to('client#view')->name('channel.view');
   $private_r->get('/:cid')->to('client#view')->name('server.view');
 
   $self->hook(before_dispatch => sub {
