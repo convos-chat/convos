3cfb2d6ab2ea1a6d01d8e7dd171e5ff6d315b0e5
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-12-20 00:08:57 +0100

    Fix many small bugs in wirc.js after rewrite
        - Fix arrow up/down in input field, when no history exists
        - Fix "badge" was missing in new_channel_template and
          new_conversation_template
        - Add poor mans testing suite. Run it with Wirc.test() in the js
          console
        - Add Wirc.Notifier.requestPermission();
        - Add fail ws sendData() will include sent message in error message
        - Move BASEURL into Wirc.base_url
        - Replace Wirc.Chat.listenToScroll() with Wirc.Chat.onScroll(). A
          lot simpler code
        - Remove Wirc.websocket()
        - Remove close button in view.html.ep for conversations. It seems to
          be invisible anyway
        - Remove $('body').click() handler to focus input field: It makes
          selecting text impossible (at least hard) in my chromium browser.
        - Remove code which is not in use
        - Remove Wirc.Chat.scrollToBottom() (did copy/paste instead...)
        - Rename Wirc.Notifier.notify() to Wirc.Notifier.show()
        - Rename Wirc.Chat.channels() to Wirc.Chat.modifyChannelList();
        - Rename Wirc.Chat.conversations() to Wirc.Chat.modifyConcersationList();

--- public/js/wirc.js
+++ public/js/wirc.js
@@ -1,102 +1,75 @@
-var BASEURL = window.location.href;
-
-(function($) {
-
 Structure.registerModule('Wirc', {
-  websocket: function(path, callbacks) {
-    var url = BASEURL.replace(/^http/, 'ws') + path;
-    var websocket = new ReconnectingWebSocket(BASEURL.replace(/^http/, 'ws') + '/socket');
-    if(window.console) console.log('[websocket] ' + url);
-    $.each(callbacks, function(name, callback) { websocket[name] = callback; });
-    return websocket;
+  base_url: window.location.href,
+  test: function() { // poor mans test suite...
+    console.log(Wirc.base_url);
+    console.log(Wirc.Chat.makeTargetId('target', 1, '#bar'));
+
+    Wirc.Notifier.show('', 'Running tests...', 'Yay!');
+    Wirc.Chat.modifyChannelList({ joined: '#too_cool', cid: 1 });
+    setTimeout(function() { Wirc.Chat.modifyChannelList({ parted: '#too_cool', cid: 1 }); }, 500);
+    Wirc.Chat.modifyConversationlist({ nick: 'caveman', cid: 1 });
+    setTimeout(function() { Wirc.Chat.displayUnread({ target: 'caveman', cid: 1 }); }, 300);
+
+    console.log(JSON.stringify(Wirc.Chat.parseIrcInput({ timestamp: 1355957508, nick: 'caveman' })));
+    console.log(JSON.stringify(Wirc.Chat.parseIrcInput({ timestamp: 1355957508, message: "\u0001ACTION what ever\u0001" })));
+    console.log(JSON.stringify(Wirc.Chat.parseIrcInput({ timestamp: 1355957508, message: 'hello ' + Wirc.Chat.nick })));
+
+    Wirc.Chat.window_has_focus = false;
+    Wirc.Chat.receiveData({ data: { cid: 1, timestamp: 1355957508, nick: 'caveman', target: Wirc.Chat.target, message: "\u0001ACTION what ever\u0001" } });
+    Wirc.Chat.receiveData({ data: { cid: 1, timestamp: 1355957508, nick: 'caveman', target: Wirc.Chat.target, message: 'hi!' } });
+    Wirc.Chat.receiveData({ data: { cid: 1, timestamp: 1355957508, nick: 'caveman', target: Wirc.Chat.target, message: 'what up ' + Wirc.Chat.nick } });
+    Wirc.Chat.receiveData({ data: { cid: 1, timestamp: 1355957508, old_nick: 'caveman', new_nick: 'cavewoman' } });
+    Wirc.Chat.window_has_focus = true;
   }
-}); // End Wirc
+});
 
 Structure.registerModule('Wirc.Notifier', {
-  init: function() {
-    var self = this;
-
-    if(!window.webkitNotifications) {
-      return self;
-    }
-    if(!webkitNotifications.checkPermission()) { // already granted
-      self.notifier = webkitNotifications;
-      return self;
-    }
-
-    // cannot run requestPermission() without a user action, such as mouse click or key down
-    $(document).one('keydown', function() {
-      webkitNotifications.requestPermission(function(e) {
-        if(window.console) console.log(e);
-        if(!webkitNotifications.checkPermission()) self.notifier = notifier;
-      });
-    });
-
-    return self;
+  show: function(icon, title, msg) {
+    if(this.notifier) this.notifier.createNotification(icon || '', title, msg || '').show();
   },
-  notify: function(icon, title, msg) {
-    if(!this.notifier) return;
-    var notification = this.notifier.createNotification(icon || '', title, msg || '');
-    notification.show();
+  requestPermission: function() {
+    webkitNotifications.requestPermission(function() {
+      if(!webkitNotifications.checkPermission()) Wirc.Notifier.notifier = notifier;
+    });
+  },
+  init: function() {
+    if(!window.webkitNotifications) {
+      // cannot show notifications
+    }
+    else if(webkitNotifications.checkPermission()) {
+      // cannot run requestPermission() without a user action, such as mouse click or key down
+      $(document).one('keydown', function() { Wirc.Notifier.requestPermission(); });
+    }
+    else {
+      this.notifier = webkitNotifications;
+    }
+    return this;
   }
 }); // End Wirc.Notify
 
 Structure.registerModule('Wirc.Chat', {
-  parseIrcMessage: function(d) {
-    var data = $.parseJSON(d);
-
-    if(data.message) {
-      var action = data.message.match(/^\u0001ACTION (.*)\u0001$/);
-      if(action) data.message = action[1];
-      data.highlight = data.message.match("\\b" + this.nick + "\\b") ? 1 : 0;
-      data.message = data.message.replace(/</i, '&lt;').replace(/\b(\w{2,5}:\/\/\S+)/g, '<a href="$1" target="_blank">$1</a>');
-      data.template = action ? 'action_message_template' : 'message_template';      
-    }    
-    if(data.timestamp) {
-      data.timestamp = new Date(parseInt(data.timestamp*1000, 10));
-    }
-
-    data.class_name = data.nick === this.nick                             ? 'me'
-                    : data.highlight                                      ? 'focus'
-                    : $('#chat_messages').find('li:last').hasClass('odd') ? 'even'
-                    :                                                       'odd';
-
-    return data;
+  makeTargetId: function() {
+    return 'target_' + $.map(arguments, function(v, i) { return v.toString().replace(/\W/g, ''); }).join('_');
   },
-  channels: function(data) {
-    if(!data) return []; // TODO: return channel names
+  modifyChannelList: function(data) { // TODO: return channel names
+    var $channel = $('#' + this.makeTargetId(data.cid, data.joined || data.parted));
 
-    var id = (data.joined || data.parted).replace(/\W/, '');
-    var $channel = $('#target_' + data.cid + '_' + id);
-
-    if(data.parted) {
+    if(data.parted)
       $channel.remove();
-    }
-    else if(data.joined && !$channel.length) {
+
+    if(data.joined && !$channel.length)
       $(tmpl('new_channel_template', data)).insertAfter('#connection_list_' + data.cid + ' .channel:last');
-    }
-
-    return this; // allow chaining
   },
-  conversations: function(data) {
-    if(!data) return []; // TODO: return conversation list
+  modifyConversationlist: function(data) {
+    var $conversation = $('#' + this.makeTargetId(data.cid, data.nick));
 
-    var id = data.nick.replace(/\W/, '');
-    var $conversation = $('#target_' + data.cid + '_' + id);
-
-    if(!$conversation.length) {
+    if(!$conversation.length)
       $(tmpl('new_conversation_template', data)).appendTo('#connection_list_' + data.cid);
-    }
-
-    return this; // allow chaining
   },
   displayUnread: function(data) {
-    var $badge = $('#target' + '_' + data.cid + '_' + channel_id + ' .badge');
+    var $badge = $('#' + this.makeTargetId(data.cid, data.target) + ' .badge');
     $badge.text(parseInt($badge.text(), 10) + 1 ).show();
     if(data.highlight) $badge.addClass('badge-important');
-  },
-  scrollToBottom: function() {
-    $('html, body').scrollTop($('body').height());
   },
   print: function(data) {
     var at_bottom = $(window).scrollTop() + $(window).height() >= $('body').height() - 30; // need to calculate at_bottom before appending a new element
@@ -118,22 +91,42 @@ Structure.registerModule('Wirc.Chat', {
     }
 
     if(at_bottom) {
-      this.scrollToBottom();
+      $('html, body').scrollTop($('body').height());
     }
   },
-  receiveData: function(e) {
-    var data = this.parseIrcMessage(e.data);
-    var channel_id;
+  parseIrcInput: function(d) {
+    var data = $.parseJSON(d) || d;
 
-    if(window.console) console.log('[websocket] > ' + e.data);
+    if(data.message) {
+      var action = data.message.match(/^\u0001ACTION (.*)\u0001$/);
+      if(action) data.message = action[1];
+      data.highlight = data.message.match("\\b" + this.nick + "\\b") ? 1 : 0;
+      data.message = data.message.replace(/</i, '&lt;').replace(/\b(\w{2,5}:\/\/\S+)/g, '<a href="$1" target="_blank">$1</a>');
+      data.template = action ? 'action_message_template' : 'message_template';
+    }
+
+    if(data.timestamp) {
+      data.timestamp = new Date(parseInt(data.timestamp * 1000, 10));
+    }
+
+    data.class_name = data.nick === this.nick                             ? 'me'
+                    : data.highlight                                      ? 'focus'
+                    : $('#chat_messages').find('li:last').hasClass('odd') ? 'even'
+                    :                                                       'odd';
+
+    return data;
+  },
+  receiveData: function(e) {
+    if(window.console) console.log('[websocket] > ', e.data);
+    var data = Wirc.Chat.parseIrcInput(e.data);
 
     // notification handling
     if(!self.window_has_focus) {
       if(data.highlight) {
-        this.notifier.notify('', 'New mention by ' + data.nick + ' in ' + data.target, data.message);
+        this.notifier.show('', 'New mention by ' + data.nick + ' in ' + data.target, data.message);
       }
       else if(data.target === this.nick) {
-        this.notifier.notify('', 'New message from ' + data.nick, data.message);
+        this.notifier.show('', 'New message from ' + data.nick, data.message);
       }
       if(data.cid == this.connection_id && data.target == this.target) {
         document.title = 'New message in ' + this.target;
@@ -142,11 +135,11 @@ Structure.registerModule('Wirc.Chat', {
 
     // action handling
     if(data.joined || data.parted) {
-      this.channels(data);
+      this.modifyChannelList(data);
     }
     else if(data.target) {
       if(data.target === this.nick && data.target !== this.target && this.target != this.nick) {
-        this.conversations(data);
+        this.modifyConversationlist(data);
       }
       if(data.target !== this.target) {
         this.displayUnread(data);
@@ -160,44 +153,30 @@ Structure.registerModule('Wirc.Chat', {
     this.print(data);
   },
   sendData: function(data) {
-    // TODO: Figure out if JSON.stringify() works in other browsers than chrome
     try {
       this.websocket.send(JSON.stringify(data));
       if(window.console) console.log('[websocket] < ' + JSON.stringify(data));
     } catch(e) {
       if(window.console) console.log('[websocket] ! ' + e);
-      this.print({ error: '[ws]' + e });
+      this.print({ message: '[ws] < (' + data + '): ' + e });
     }
   },
-  listenToScroll: function() {
-    var $win = $(window);
-    var $messages = $('#chat_messages');
-    var $loading;
-    var page = 1;
-    var height;
-
-    $win.on('scroll', function() {
-      if($loading || $win.scrollTop() !== 0) return;
-      $loading = $('<div class="alert alert-info">Loading previous conversations...</div>');
-      height = $('body').height();
-      page++;
-      $messages.before($loading);
-      if(window.console) console.log(BASEURL + '/history/' + page);
-      $.ajax({
-        url: BASEURL + '/history/' + page,
-        success: function(data) {
-          var $li = $(data).find('#chat_messages li');
-          if($li.length) {
-            $messages.prepend($li);
-            $loading.remove();
-            $loading = false;
-            $win.scrollTop($('body').height() - height);
-          }
-          else {
-            $loading.removeClass('alert-info').text('End of conversation log.');
-          }
-        }
-      });
+  onScroll: function() {
+    if(this.$history_indicator || $(window).scrollTop() !== 0) return;
+    var height_before_load = $('body').height();
+    this.$history_indicator = $('<div class="alert alert-info">Loading previous conversations...</div>');
+    this.$messages.before($history_indicator);
+    $.get(Wirc.base_url + '/history/' + (++this.history_index), function(data) {
+      var $li = $(data).find('#chat_messages li');
+      if($li.length) {
+        this$messages.prepend($li);
+        this.$history_indicator.remove();
+        this.$history_indicator = false;
+        $(window).scrollTop($('body').height() - height_before_load);
+      }
+      else {
+        $history_indicator.removeClass('alert-info').text('End of conversation log.');
+      }
     });
   },
   init: function($) {
@@ -205,32 +184,29 @@ Structure.registerModule('Wirc.Chat', {
     var original_title = document.title;
 
     self.$messages = $('#chat_messages');
-    self.connection_id = $('#chat_messages').attr('data-cid');
-    self.nick = $('#chat_messages').attr('data-nick');
-    self.target = $('#chat_messages').attr('data-target');
-    self.input = Wirc.Chat.Input.init($('#chat_input_field input[type="text"]'));
+    self.connection_id = self.$messages.attr('data-cid');
+    self.nick = self.$messages.attr('data-nick');
+    self.target = self.$messages.attr('data-target');
+    self.history_index = 0;
     self.notifier = Wirc.Notifier.init();
-    self.scrollToBottom();
-    self.listenToScroll();
 
-    self.websocket = Wirc.websocket('/socket', {
-      onmessage: self.receiveData,
-      onopen: function function_name (argument) {
-        self.sendData({ cid: self.connection_id, target: self.target });
-      }
-    });
+    self.websocket = new ReconnectingWebSocket(Wirc.base_url.replace(/^http/, 'ws') + '/socket');
+    self.websocket.onopen = function() { self.sendData({ cid: self.connection_id, target: self.target }); };
+    self.websocket.onmessage = self.receiveData;
 
+    self.input = Wirc.Chat.Input.init($('#chat_input_field input[type="text"]'));
     self.input.submit = function(e) {
       self.sendData({ cid: self.connection_id, target: self.target, cmd: this.$input.val() });
       this.$input.val(''); // TODO: Do not clear the input field until echo is returned?
       return false;
     };
-
     $.each($('#chat_messages').attr('data-nicks').split(','), function(i, v) {
       if(v == this.nick) return;
       self.input.autoCompleteNicks({ new_nick: v.replace(/^\@/, '') });
     });
 
+    $('html, body').scrollTop($('body').height());
+    $(window).on('scroll', Wirc.Chat.onScroll);
     $(window).blur(function() { self.window_has_focus = false; });
     $(window).focus(function() { self.window_has_focus = true; document.title = original_title; });
     if(window.console) console.log('[Wirc.Chat.init] ', self);
@@ -291,6 +267,7 @@ Structure.registerModule('Wirc.Chat.Input', {
       switch(e.keyCode) {
         case 38: // up
           e.preventDefault();
+          if(self.history.length === 0) return;
           if(self.history_index == self.history.length) self.initial_value = this.value;
           if(--self.history_index < 0) self.history_index = 0;
           this.value = self.history[self.history_index];
@@ -298,6 +275,7 @@ Structure.registerModule('Wirc.Chat.Input', {
 
         case 40: // down
           e.preventDefault();
+          if(self.history.length === 0) return;
           if(++self.history_index >= self.history.length) self.history_index = self.history.length;
           this.value = self.history[self.history_index] || self.initial_value || '';
           break;
@@ -331,17 +309,15 @@ Structure.registerModule('Wirc.Chat.Input', {
     $input.parents('form').submit(function(e) { return self.submit(e); });
     $input.focus();
 
-    $('body').click(function() { $input.focus(); });
-
     return self;
   }
 }); // End Wirc.Chat.Input
 
-$(document).ready(function() {
-  BASEURL = $('script[src$="jquery.js"]').get(0).src.replace(/\/js\/[^\/]+$/, '');
-  $('#chat_messages').each(function() { setTimeout(function() { Wirc.Chat.init($); }, 100); });
-});
-
+(function($) {
+  $(document).ready(function() {
+    Wirc.base_url = $('script[src$="jquery.js"]').get(0).src.replace(/\/js\/[^\/]+$/, '');
+    $('#chat_messages').each(function() { setTimeout(function() { Wirc.Chat.init($); }, 100); });
+  });
 })(jQuery);
 
 /*
@@ -349,13 +325,13 @@ $(document).ready(function() {
  *
 if(!('WebSocket' in window)) {
   document.write([
-    '<script type="text/javascript" src="' + BASEURL + '/js/swfobject.js"></script>',
-    '<script type="text/javascript" src="' + BASEURL + '/js/FABridge.js"></script>',
-    '<script type="text/javascript" src="' + BASEURL + '/js/web_socket.js"></script>'
+    '<script type="text/javascript" src="' + Wirc.base_url + '/js/swfobject.js"></script>',
+    '<script type="text/javascript" src="' + Wirc.base_url + '/js/FABridge.js"></script>',
+    '<script type="text/javascript" src="' + Wirc.base_url + '/js/web_socket.js"></script>'
   ].join(''));
 }
 if(WebSocket.__initialize) {
   // Set URL of your WebSocketMain.swf here:
-  WebSocket.__swfLocation = BASEURL + '/js/WebSocketMain.swf';
+  WebSocket.__swfLocation = Wirc.base_url + '/js/WebSocketMain.swf';
 }
 */
