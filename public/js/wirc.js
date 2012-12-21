23bd5f0eabd5026936c63debc1f58536dcf00bcb
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-12-21 11:22:19 +0100

    Add test for loading chat history

--- public/js/wirc.js
+++ public/js/wirc.js
@@ -10,15 +10,17 @@ Structure.registerModule('Wirc', {
     Wirc.Chat.modifyConversationlist({ nick: 'caveman', cid: 1 });
     setTimeout(function() { Wirc.Chat.displayUnread({ target: 'caveman', cid: 1 }); }, 300);
 
-    console.log(JSON.stringify(Wirc.Chat.parseIrcInput({ timestamp: 1355957508, nick: 'caveman' })));
-    console.log(JSON.stringify(Wirc.Chat.parseIrcInput({ timestamp: 1355957508, message: "\u0001ACTION what ever\u0001" })));
-    console.log(JSON.stringify(Wirc.Chat.parseIrcInput({ timestamp: 1355957508, message: 'hello ' + Wirc.Chat.nick })));
+    console.log(JSON.stringify(Wirc.Chat.parseIrcInput('{"timestamp":1355957508,"nick":"caveman"}')));
+    console.log(JSON.stringify(Wirc.Chat.parseIrcInput('{"timestamp":1355957508,"message":"\\u0001ACTION what ever\\u0001"}')));
+    console.log(JSON.stringify(Wirc.Chat.parseIrcInput('{"timestamp":1355957508,"message":"hello ' + Wirc.Chat.nick + '"}')));
+
+    Wirc.Chat.onScroll();
 
     Wirc.Notifier.window_has_focus = false;
-    Wirc.Chat.receiveData({ data: { cid: 1, timestamp: 1355957508, nick: 'caveman', target: Wirc.Chat.target, message: "\u0001ACTION what ever\u0001" } });
-    Wirc.Chat.receiveData({ data: { cid: 1, timestamp: 1355957508, nick: 'caveman', target: Wirc.Chat.target, message: 'hi!' } });
-    Wirc.Chat.receiveData({ data: { cid: 1, timestamp: 1355957508, nick: 'caveman', target: Wirc.Chat.target, message: 'what up ' + Wirc.Chat.nick } });
-    Wirc.Chat.receiveData({ data: { cid: 1, timestamp: 1355957508, old_nick: 'caveman', new_nick: 'cavewoman' } });
+    Wirc.Chat.receiveData({ data: '{"cid":1,"timestamp":1355957508,"nick":"caveman","target":"' + Wirc.Chat.target + '","message":"\\u0001ACTION what ever\\u0001"}' });
+    Wirc.Chat.receiveData({ data: '{"cid":1,"timestamp":1355957508,"nick":"caveman","target":"' + Wirc.Chat.target + '","message":"hi!"}' });
+    Wirc.Chat.receiveData({ data: '{"cid":1,"timestamp":1355957508,"nick":"caveman","target":"' + Wirc.Chat.target + '","message":"what up ' + Wirc.Chat.nick + '"}' });
+    Wirc.Chat.receiveData({ data: '{"cid":1,"timestamp":1355957508,"old_nick":"caveman","new_nick":"cavewoman"}' });
     Wirc.Notifier.window_has_focus = true;
 
     // Need to unfocus the window to make this run. Should blink the tab
@@ -119,7 +121,7 @@ Structure.registerModule('Wirc.Chat', {
     }
   },
   parseIrcInput: function(d) {
-    var data = $.parseJSON(d) || d;
+    var data = $.parseJSON(d);
 
     if(data.message) {
       var action = data.message.match(/^\u0001ACTION (.*)\u0001$/);
@@ -186,9 +188,11 @@ Structure.registerModule('Wirc.Chat', {
     if(this.$history_indicator || $(window).scrollTop() !== 0) return;
     var self = this;
     var height_before_load = $('body').height();
+    var url = Wirc.base_url + '/history/' + (++self.history_index);
     self.$history_indicator = $('<div class="alert alert-info">Loading previous conversations...</div>');
-    self.$messages.before($self.history_indicator);
-    $.get(Wirc.base_url + '/history/' + (++self.history_index), function(data) {
+    self.$messages.before(self.$history_indicator);
+    if(window.console) console.log('[Wirc.Chat.onScroll] ' + url);
+    $.get(url, function(data) {
       var $li = $(data).find('#chat_messages li');
       if($li.length) {
         self.$messages.prepend($li);
@@ -208,7 +212,7 @@ Structure.registerModule('Wirc.Chat', {
     self.connection_id = self.$messages.attr('data-cid');
     self.nick = self.$messages.attr('data-nick');
     self.target = self.$messages.attr('data-target');
-    self.history_index = 0;
+    self.history_index = 1;
     self.notifier = Wirc.Notifier.init();
 
     self.websocket = new ReconnectingWebSocket(Wirc.base_url.replace(/^http/, 'ws') + '/socket');
