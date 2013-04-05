d5bad0c7789e73bf5e10b41d6b02918ced1cd6f2
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-04-05 12:28:42 +0200

    37: Drafted new user interface
        This user interface is not like previous. It has:
        - A navbar with...
          * conversation name
          * Link to unread conversation menu
          * Link to all conversations menu
          * Link to settings
        - The input and conversation area takes full width
    
        TODO:
        - initShortcuts() probably need new shortcuts
        - Need to differ between important and unimportant unread messages
        - Make settings page look good
        - Load title from channel into navbar

--- public/js/wirc.js
+++ public/js/wirc.js
@@ -1,24 +1,22 @@
 (function($) {
   var input_selector = '.chat form input[type="text"]';
-  var messages_selector = '.messages ul';
+  var messages_selector = '#messages ul';
   var at_bottom = true;
-  var websocket, history_index, $conversation, $connection_list;
+  var websocket, history_offset, $conversation, $connection_list;
 
   var methods = {
     init: function() {
       websocket = new ReconnectingWebSocket($.url_for('socket').replace(/^http/, 'ws'));
       websocket.onmessage = methods.receiveData;
 
-      $connection_list = $('.conversation-list');
-
       methods.changeChannel();
+      methods.initNavbar();
       methods.initPjax();
       methods.initShortcuts();
       methods.onResize();
       notifier.init();
 
       $('.embed img').live('click', function() { $(this).remove(); });
-      $('a.show-hide').fastclick(function() { $connection_list.toggleClass('hidden open'); return false; });
       $(input_selector).chatInput().parents('form').submit(methods.onSubmit);
       $(window).resize(methods.onResize);
 
@@ -29,22 +27,50 @@
       log('Wirc.Chat.init() success');
       return this;
     },
+    initNavbar: function() {
+      var $connection_list = $('#connection_list > .dropdown-menu');
+      var hide = function(e) {
+          e.preventDefault();
+          $connection_list.hide();
+          $('#navbar').find('a').parent('li').removeClass('open');
+          $('body').unbind('click', hide);
+      };
+      var show = function(e) {
+        var $li = $(this).parent('li:first');
+        hide.call(this, e);
+        $connection_list.css('right', $(window).width() - $li.offset().left - $li.width() - 43).show();
+        $li.addClass('open');
+        $('body').one('click', hide);
+      };
+
+      $('#navbar .unread-menu').click(function(e) {
+        if($(this).parent('li:first').hasClass('open')) return hide.call(this, e);
+        $connection_list.find('.channel, .conversation').each(function() {
+          if($(this).find('.badge').text() == '0') $(this).hide();
+        });
+        show.call(this, e);
+        return false;
+      });
+      $('#navbar .chat-menu').click(function(e) {
+        if($(this).parent('li:first').hasClass('open')) return hide.call(this, e);
+        $connection_list.find('.channel, .conversation').show();
+        show.call(this, e);
+        return false;
+      });
+    },
     initPjax: function() {
       $(document).on('pjax:send', function(event) {
-        $(event.relatedTarget).parent('li:first').addClass('loading');
         statusIndicator('show', 'Loading...');
       });
-      $(document).on('pjax:complete', function(event) {
-        $(event.relatedTarget).parent('li:first').removeClass('loading');
-      });
       $(document).on('pjax:timeout', function(event) {
+        statusIndicator('show', 'Timeout');
         event.preventDefault(); // Prevent default timeout redirection behavior
       });
-      $('#conversation').on('pjax:end', function(e) {
+      $('#messages > div').on('pjax:end', function(e) {
         statusIndicator('fadeOut');
         methods.changeChannel();
       });
-      $(document).pjax('.server a', '#conversation');
+      $(document).pjax('#connection_list a', '#messages > div');
     },
     initShortcuts: function() {
       var $input = $('input');
@@ -54,14 +80,14 @@
       });
       $input.bind('keydown', 'ctrl+up', function(e) {
         e.preventDefault();
-        $('.conversation-list li.active').prev().find('a').click();
+        $('#connection_list li.active').prev().find('a').click();
       });
       $input.bind('keydown', 'ctrl+down', function(e) {
         e.preventDefault();
-        $('.conversation-list li.active').next().find('a').click();
+        $('#connection_list li.active').next().find('a').click();
       });
       $input.bind('keydown', 'ctrl+shift+up', function() {
-        $('.conversation-list li.active').prevAll().each(function(i) {
+        $('#connection_list li.active').prevAll().each(function(i) {
           if($(this).find('.badge:visible').length) {
             $(this).find('a').click();
             return false;
@@ -69,7 +95,7 @@
         });
       });
       $input.bind('keydown', 'ctrl+shift+down', function() {
-        $('.conversation-list li.active').nextAll().each(function(i) {
+        $('#connection_list li.active').nextAll().each(function(i) {
           if($(this).find('.badge:visible').length) {
             $(this).find('a').click();
             return false;
@@ -82,12 +108,13 @@
       return escaped ? target.replace(/:/g, '\\:') : target;
     },
     changeChannel: function() {
-      $conversation = $('#conversation > ul');
+      $conversation = $('#messages ul:first');
       history_offset = $conversation.attr('data-offset');
       $(window).scrollToBottom();
       $(input_selector).chatInput('initAutocomplete', $conversation.attr('data-nicks').replace(/\@/g, '').split(','));
-      $('.server li').removeClass('active');
+      $('#connection_list li').removeClass('active');
       $('#target_' + methods.activeTarget(1)).addClass('active').find('.badge').text('0').removeClass('badge-important').hide();
+      methods.unread('init');
       log('changeChannel', $conversation.attr('id'));
     },
     printMessage: function(target) {
@@ -100,14 +127,12 @@
           $(input_selector).chatInput('removeAutocomplete', this.attr('data-nick'));
         if(at_bottom)
           $(window).scrollToBottom();
-        this.find('img').once('load', function() {
+        this.find('img').one('load', function() {
           if(at_bottom) $(window).scrollToBottom();
         });
       }
       else if(this.hasClass('message')) {
-        var $badge = $('#target_' + target + ' .badge');
-        $badge.text(parseInt($badge.text(), 10) + 1).show();
-        if(this.hasClass('highlight')) $badge.addClass('badge-important');
+        methods.unread(this.hasClass('highlight') ? 'important' : 'normal', target);
       }
     },
     receiveData: function(e) {
@@ -145,28 +170,45 @@
 
       methods.printMessage.call($data, target);
     },
-    onResize: function() {
-      if(at_bottom) $(window).scrollToBottom();
-      if($(window).width() < 767) {
-        if(!$connection_list.hasClass('open')) $connection_list.addClass('hidden');
+    unread: function(action, target) {
+      if(action == 'init') {
+        var method, n = 0;
+        $('#connection_list span.badge').each(function() {
+          n += parseInt($(this).text(), 10);
+        });
+        $('#navbar .unread-menu .badge-unimportant').text(n);
+        $('#navbar .unread-menu .badge-important').text(0); // TODO: This should be calculated like unimportant
       }
       else {
-        $connection_list.removeClass('hidden');
+        var $badge = $('#target_' + target + ' .badge');
+        $badge.text(parseInt($badge.text(), 10) + 1).show();
+        if(action == 'important') {
+          $badge.addClass('badge-important');
+          $badge = $('#navbar .unread-menu .badge-important');
+          $badge.text(parseInt($badge.text(), 10) + 1);
+        }
+        else {
+          $badge = $('#navbar .unread-menu .badge-unimportant');
+          $badge.text(parseInt($badge.text(), 10) + 1);
+        }
       }
     },
+    onResize: function() {
+      var h = $(window).height() - 60;
+      $('#connection_list .dropdown-menu').css({ 'max-height': h });
+    },
     onScroll: function() {
-      at_bottom = $(window).atBottom(); // need to calculate at_bottom before appending a new element
-      if(!history_index || statusIndicator()) return;
+      if(!history_offset || statusIndicator()) return;
       if($(window).scrollTop() !== 0) return;
       statusIndicator('show', 'Loading previous conversations...');
       $.get($.url_for('v1', methods.activeTarget(0), 'history', history_offset), function(data) {
         var $data = $(data);
-        if($data.find('*').length) {
+        if($data.children('li').length) {
           var height_before_prepend = $('body').height();
-          history_offset = $data.attr('data-offset');
-          $(messages_selector).prepend(data);
+          $(messages_selector).prepend($data.children('li'));
           $(window).scrollTop($('body').height() - height_before_prepend);
           statusIndicator('fadeOut');
+          history_offset = $data.attr('data-offset');
         }
         else {
           history_offset = 0;
@@ -197,6 +239,6 @@
   };
 
   $(document).ready(function() {
-    if($('body.chat .messages').length) $(document).wircChat();
+    if($('#messages').length) $(document).wircChat();
   });
 })(jQuery);
