4c8f0bbcc378d3bd2e7a4e88f81213928fba9f95
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-07-28 11:29:03 +0200

    Add ugly debugging to screen

--- public/js/jquery.wirc.js
+++ public/js/jquery.wirc.js
@@ -105,6 +105,11 @@ window.sortedSet.prototype.revrange = function(start, stop) {
 
 // console.log()
 window.console = window.console || { log: function() { window.console.messages.push(arguments) }, messages: [] };
+window.console = {
+  log: function() {
+    $('body').append('<br> DEBUG: ' + jQuery.makeArray(arguments).join(' '));
+  }
+};
 
 // this code is originally from https://github.com/joewalnes/reconnecting-websocket
 window.ws = function(a) {
@@ -123,8 +128,8 @@ window.ws = function(a) {
       if (b.debug) console.debug("ReconnectingWebSocket", "onopen", a);
       b.readyState = WebSocket.OPEN;
       g = false;
+      while(b.buffer.length) b.send(b.buffer.shift());
       b.onopen(c);
-      while(b.buffer.length) h.send(b.buffer.shift());
     };
     c.onclose = function(h) {
       clearTimeout(i);
@@ -161,15 +166,17 @@ window.ws = function(a) {
     onmessage: function(m) { console.log(b.url + ' < ' + m); },
     onclose: function(e) { console.log(b.url + ' : close'); },
     reconnectInterval: 1e3,
-    timeoutInterval: 2e3,
+    timeoutInterval: 5e3,
     readyState: WebSocket.CONNECTING,
     url: a,
     close: function() { if(!c) return false; c.close(); return(d = true); },
     send: function(m) {
       if(b.readyState == WebSocket.OPEN) {
+        $('body').append('OK: ' + $(m).text());
         c.send(m);
       }
       else {
+        $('body').append('QUE: ' + $(m).text());
         b.buffer.push(m);
       }
       return b
