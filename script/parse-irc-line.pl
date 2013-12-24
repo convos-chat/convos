68ca0c8dedc5f8c3cc3c30a4bced3b2b7e438e3b
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-12-24 15:10:06 +0100

    Add sorted output to parse-irc-line.pl

--- script/parse-irc-line.pl
+++ script/parse-irc-line.pl
@@ -4,6 +4,9 @@ use Parse::IRC;
 use IRC::Utils;
 BEGIN { warn "Paste in a line of raw IRC...\n" }
 my $d = parse_irc($_);
-$d->{'command_name'} = IRC::Utils::numeric_to_name($d->{'command'}) || '';
-$d->{'command_name_lc'} = lc(IRC::Utils::numeric_to_name($d->{'command'}) || '');
+$d->{command_name} = IRC::Utils::numeric_to_name($d->{command}) || '';
+$d->{command_name_lc} = lc(IRC::Utils::numeric_to_name($d->{command}) || '');
+local $Data::Dumper::Indent = 1;
+local $Data::Dumper::Sortkeys = 1;
+local $Data::Dumper::Terse = 1;
 print Dumper($d);
