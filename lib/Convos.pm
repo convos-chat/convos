74b67159962ee8a8c2b523511a9b7e0328733039
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-01-03 03:32:15 +0100

    Adding cpanfile to CPAN dist
    Conflicts:
    	lib/Convos.pm

--- lib/Convos.pm
+++ lib/Convos.pm
@@ -6,7 +6,7 @@ Convos - Multiuser IRC proxy with web interface
 
 =head1 VERSION
 
-0.3
+0.3004
 
 =head1 DESCRIPTION
 
@@ -129,7 +129,7 @@ use Convos::Core;
 use Convos::Core::Util ();
 use Convos::Upgrader;
 
-our $VERSION = '0.3';
+our $VERSION = '0.3004';
 $ENV{CONVOS_BACKEND_REV} ||= 0;
 
 =head1 ATTRIBUTES
