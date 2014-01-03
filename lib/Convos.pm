ec9ad5d6c73ba7a92d09979a2eb9186a07ef95c2
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-01-03 09:20:08 +0100

    Need convos.testing.conf as well with the dist

--- lib/Convos.pm
+++ lib/Convos.pm
@@ -6,7 +6,7 @@ Convos - Multiuser IRC proxy with web interface
 
 =head1 VERSION
 
-0.3004
+0.3005
 
 =head1 DESCRIPTION
 
@@ -128,7 +128,7 @@ use File::Basename qw( dirname );
 use Convos::Core;
 use Convos::Core::Util ();
 
-our $VERSION = '0.3004';
+our $VERSION = '0.3005';
 $ENV{CONVOS_BACKEND_REV} ||= 0;
 
 =head1 ATTRIBUTES
