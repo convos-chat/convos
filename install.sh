3592e1b9f080a49b8987892e807ddb09074cd0bf
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-01-07 11:48:47 +0100

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+CURL=$(which curl);
+PERL=$(which perl);
+WGET=$(which wget);
+TAR_GZ="https://codeload.github.com/Nordaaker/convos/tar.gz/release";
+
+if [ -z $PERL ]; then
+  echo "! Cannot install Convos: Cannot find 'perl' in PATH";
+  exit 2;
+fi
+
+function download () {
+  if [ -n $CURL ]; then
+    curl $TAR_GZ | tar zxvf -
+  elif [ -n $WGET ]; then
+    wget $TAR_GZ -O - | tar zxvf -
+  else
+    echo "! Cannot install Convos: Cannot find 'curl' or 'wget' in PATH";
+    exit 2;
+  fi
+}
+
+function deps () {
+  cd convos-release;
+
+  if ./vendor/bin/carton; then
+    echo "";
+    echo "# Convos was installed successfully!";
+  else
+    EXIT_VALUE=$?
+    echo "! Carton could not install dependencies. ($EXIT_VALUE)";
+    exit $EXIT_VALUE;
+  fi
+}
+
+function post_message () {
+  echo "";
+  echo "# You can test convos by running the command below,";
+  echo "# and then open http://localhost:3000 in your favorite browser.";
+  echo "";
+  echo "  cd $PWD && ./vendor/bin/carton exec morbo script/convos";
+  echo "";
+  echo "# Visit http://convos.by for more information.";
+  echo "";
+}
+
+download;
+deps;
+post_message;
+
+exit 0;
