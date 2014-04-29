78426c0b26945572d420b5a8d61c149ff5ccf764
Dominik Tobschall <dominik@fruux.com>
2014-04-29 12:35:27 +0200

    don't render the sign-up form at all if invite code is incorrect

--- lib/Convos/User.pm
+++ lib/Convos/User.pm
@@ -231,11 +231,11 @@ sub register {
 
   $self->stash(form => 'register');
 
-  if ($self->req->method ne 'POST') {
-    return $self->render('index');
-  }
   if ($invite_code and $invite_code ne ($validation->input->{invite} || '')) {
     return $self->render('index', form => 'invite_only', status => 400);
+  }
+  if ($self->req->method ne 'POST') {
+    return $self->render('index');
   }
 
   $validation->required('login')->like(qr/^\w+$/)->size(3, 15);
