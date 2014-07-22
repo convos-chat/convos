9d8127b7cea3a29a1d64351422fe2c3d3a84b0b9
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-07-22 17:14:45 +0200

    Cleaning up script/

--- script/dev-mode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-[ -z $CONVOS_DEBUG ]     && export CONVOS_DEBUG=1
-[ -z $MOJO_REDIS_DEBUG ] && export MOJO_REDIS_DEBUG=1
-[ -z $MOJO_IRC_DEBUG ]   && export MOJO_IRC_DEBUG=1
-
-exec morbo script/convos -w public/sass -w lib -w templates $@
