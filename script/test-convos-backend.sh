587517aa2b55049a5473731b1900b0e90e78424e
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-09-06 11:41:06 +0200

    Merge test mode config into script/convos

--- script/test-convos-backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-[ -z $CONVOS_DEBUG ] && export CONVOS_DEBUG=1;
-[ -z $MOJO_REDIS_DEBUG ] && export MOJO_REDIS_DEBUG=1;
-[ -z $MOJO_IRC_DEBUG ] && export MOJO_IRC_DEBUG=1;
-[ -z $CONVOS_REDIS_URL ] && export CONVOS_REDIS_URL="localhost";
-
-exec script/convos backend $@;
