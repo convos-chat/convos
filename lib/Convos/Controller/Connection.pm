d33b1b3ecbe5500f4556ff68bc2a61ebcfae09bf
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-06-13 22:13:47 +0200

    Add POST /1.0/connection

--- lib/Convos/Controller/Connection.pm
+++ lib/Convos/Controller/Connection.pm
@@ -12,8 +12,47 @@ user related actions.
 =cut
 
 use Mojo::Base 'Mojolicious::Controller';
+use constant DEBUG => $ENV{CONVOS_DEBUG} || 0;
 
 =head1 METHODS
+
+=head2 connection_add
+
+See L<Convos::Manual::API/connectionAdd>.
+
+=cut
+
+sub connection_add {
+  my ($self, $args, $cb) = @_;
+  my $user = $self->backend->user or return $self->unauthorized($cb);
+  my @userinfo = map { $_ // '' } @{$args->{data}}{qw( username password )};
+  my $url      = Mojo::URL->new($self->_connection_url(@{$args->{data}}{qw( protocol server )}));
+  my $name     = $self->_pretty_connection_name($url->host);
+  my $connection;
+
+  unless ($name) {
+    return $self->$cb($self->invalid_request('Server need a valid host.', '/data/server'), 400);
+  }
+
+  eval {
+    $url->userinfo(join ':', @userinfo)
+      if grep {length} @userinfo;
+    $connection = $user->connection($args->{data}{protocol}, $name);
+    $connection->url->parse($url);
+  } or do {
+    warn $@ if DEBUG;
+    return $self->$cb($self->invalid_request('Could not find class from protocol.', '/data/protocol'), 400);
+  };
+
+  $self->delay(
+    sub { $connection->save(shift->begin) },
+    sub {
+      my ($delay, $err) = @_;
+      die $err if $err;
+      $self->$cb($connection->TO_JSON, 200);
+    },
+  );
+}
 
 =head2 connections
 
@@ -31,6 +70,27 @@ sub connections {
   }
 
   $self->$cb(\@connections, 200);
+}
+
+sub _pretty_connection_name {
+  my ($self, $name) = @_;
+
+  return '' unless defined $name;
+  return 'magnet' if $name =~ /\birc\.perl\.org\b/i;    # also match ssl.irc.perl.org
+  return 'efnet'  if $name =~ /\befnet\b/i;
+
+  $name =~ s!^(irc|chat)\.!!;                           # remove common prefixes from server name
+  $name =~ s!:\d+$!!;                                   # remove port
+  $name =~ s!\.\w{2,3}$!!;                              # remove .com, .no, ...
+  $name =~ s![\W_]+!-!g;                                # make pretty url
+  $name;
+}
+
+sub _connection_url {
+  my ($self, $protocol, $server) = @_;
+
+  return $server if $server =~ m!^\w+://!;
+  return sprintf '%s://%s', lc $protocol, $server;
 }
 
 =head1 COPYRIGHT AND LICENSE
