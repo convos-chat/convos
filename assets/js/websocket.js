9f9aa4f2addb8c63572eab1847d8f6572a1a6a25
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-10-11 16:29:35 +0200

    Improved reconnect code in websocket.js

--- assets/js/websocket.js
+++ assets/js/websocket.js
@@ -1,17 +1,19 @@
 (function() {
   window.ReconnectingWebSocket = function(url) {
     this.readyState = WebSocket.CLOSED;
-    this.reconnectInterval = 1000;
+    this._reconnectIn = 500;
     this.url = url;
     riot.observable(this);
   };
 
   var proto = window.ReconnectingWebSocket.prototype;
 
+  // Close the connection to the WebSocket
+  // ws = ws.close(1000, reason);
   proto.close = function(code, reason) {
     if (this._tid) clearTimeout(this._tid);
     if (this._ws  && self._ws.readyState != WebSocket.CLOSED) {
-      if (window.DEBUG) console.log('[ReconnectingWebSocket] close ' + code + '/' + reason);
+      if (window.DEBUG) console.log('[ReconnectingWebSocket] Close ' + code + '/' + reason);
       this._ws.close(code || 1000, reason);
       this.readyState = WebSocket.CLOSING;
     }
@@ -21,47 +23,68 @@
     return this;
   };
 
+  // Open a connection to the WebSocket
+  // ws = ws.open(function() { ... });
+  // ws = ws.open();
   proto.open = function(cb) {
-    var self = this;
-
     if (this._ws && this._ws.readyState != WebSocket.CLOSED) {
       if (cb && this._ws.readyState == WebSocket.OPEN) cb.call(this);
       return this;
     }
     if (cb) this.one('open', cb);
-    if (window.DEBUG) console.log('[ReconnectingWebSocket] open ' + this.url);
+    if (window.DEBUG) console.log('[ReconnectingWebSocket] Open ' + this.url);
 
     this.readyState = WebSocket.CONNECTING;
-    this._tid = setTimeout(this.open.bind(this), this.reconnectInterval);
+    this._reconnect(true);
     this._ws = new WebSocket(this.url);
-    this._ws.onerror = function(e) { console.log('[ReconnectingWebSocket] error:', e); };
+    this._ws.onerror = function(e) { this._reconnect(true); }.bind(this);
     this._ws.onmessage = function(e) {
-      self.trigger('message', e);
-      if (e.data.match(/^[\{\[]/)) self.trigger('json', JSON.parse(e.data));
-    };
-    this._ws.onopen = function() { self.readyState = WebSocket.OPEN; self.trigger('open'); };
+      this.trigger('message', e);
+      if (e.data.match(/^[\{\[]/)) this.trigger('json', JSON.parse(e.data));
+    }.bind(this);
+    this._ws.onopen = function() {
+      this._reconnectIn = 500;
+      this.readyState = WebSocket.OPEN;
+      this.trigger('open');
+    }.bind(this);
     this._ws.onclose = function(e) {
-      var prevState = self.readyState;
-      self.readyState = this.readyState == WebSocket.CLOSING ? WebSocket.CLOSED : WebSocket.CONNECTING;
-      if (window.DEBUG) console.log('[ReconnectingWebSocket] close ', e);
-      if (e.code != 1000) self.trigger('error', e);
-      if (prevState == WebSocket.OPEN) self.trigger('close', e);
-      delete self._ws;
-      self._tid = setTimeout(self.open.bind(self), self.reconnectInterval);
-    };
+      var prevState = this.readyState;
+      this.readyState = this._ws.readyState == WebSocket.CLOSING ? WebSocket.CLOSED : WebSocket.CONNECTING;
+      if (window.DEBUG) console.log('[ReconnectingWebSocket] ' + _name(prevState) + ' => ' + _name(this.readyState), e);
+      if (prevState == WebSocket.OPEN) this.trigger('close', e);
+      if (this.readyState == WebSocket.CONNECTING) this._reconnect(false);
+      delete this._ws;
+    }.bind(this);
 
     return this;
   };
 
+  // Send data to the WebSocket
+  // ws = ws.send(str);
+  // ws = ws.send({foo: 123});
   proto.send = function(data) {
     var ws = this._ws;
     if (typeof data == 'object') data = JSON.stringify(data);
     if (ws && ws.readyState == WebSocket.OPEN) return ws.send(data);
-    ws.trigger('error', {code: 1003, reason: 'Cannot send message when not connected.'});
+    throw 'Cannot send message when not connected.';
   };
 
-  // bool = obj.stateIs({CONNECTING,CLOSED,CLOSING,OPEN});
-  proto.stateIs = function(state) {
-    return this.readyState == WebSocket[state];
+  proto._reconnect = function(force) {
+    if (!force && this._tid) return;
+    if (this._tid) clearTimeout(this._tid);
+    if (this._reconnectIn > 6000) this._reconnectIn = 500;
+    if (window.DEBUG == 2) console.log('[ReconnectingWebSocket] Reconnect after ' + this._reconnectIn);
+    this._tid = setTimeout(function() { delete this._tid; this.open() }.bind(this), this._reconnectIn);
+    this._reconnectIn += 1000;
   };
+
+  var _name = function(s) {
+    switch(s) {
+      case WebSocket.CLOSED:     return 'CLOSED';
+      case WebSocket.CLOSING:    return 'CLOSING';
+      case WebSocket.CONNECTING: return 'CONNECTING';
+      case WebSocket.OPEN:       return 'OPEN';
+      default:                   return s;
+    }
+  }
 })();
