6b6c978575bb1c91c5ca6fb549618aadbe53e71a
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-02-26 22:12:28 +0100

    Extending JavaScript Date prototype with new methods

--- assets/js/mixins/time.js
+++ assets/js/mixins/time.js
@@ -3,46 +3,17 @@
   var ONE_HOUR = ONE_MINUTE * 60;
   var ONE_DAY = ONE_HOUR * 24;
 
-  var extendedDate = function(str) {
-    var t = new Date(str);
-
-    t.getAbbrMonth = function() {
-      switch(this.getMonth()) {
-        case 0: return 'Jan';
-        case 1: return 'Feb';
-        case 2: return 'March';
-        case 3: return 'Apr';
-        case 4: return 'May';
-        case 5: return 'Jun';
-        case 6: return 'July';
-        case 7: return 'Aug';
-        case 8: return 'Sept';
-        case 9: return 'Oct';
-        case 10: return 'Nov';
-        case 11: return 'Dec';
-      }
-    };
-
-    t.getHm = function() {
-      return [this.getHours(), this.getMinutes()].map(function(v) { return v < 10 ? '0' + v : v; }).join(':');
-    };
-
-    return t;
-  };
-
   proto.timestring = function(ts) {
     if (!ts) return '';
-    var now = new Date().getTime() / 1000;
+    if (typeof ts == 'string') ts = new Date(ts);
+    var now = new Date().epoch();
     var yesterday = now - (now % 86400);
     var tomorrow = now + 86400 - (now % 86400);
-    var s;
+    var s = ts.epoch();
 
-    ts = extendedDate(ts);
-    s = ts.getTime() / 1000;
-
-    if (s > tomorrow + 86400)    return ts.getDate() + '. ' + ts.getAbbrMonth() + ' ' + ts.getHm();
-    if (s > yesterday)           return ts.getHm();
-    if (s > now - ONE_DAY * 31)  return ts.getDate() + '. ' + ts.getAbbrMonth() + ' ' + ts.getHm();
+    if (s > tomorrow + 86400)    return ts.getDate() + '. ' + ts.getAbbrMonth() + ' ' + ts.getHM();
+    if (s > yesterday)           return ts.getHM();
+    if (s > now - ONE_DAY * 31)  return ts.getDate() + '. ' + ts.getAbbrMonth() + ' ' + ts.getHM();
     return ts.getDate() + '. ' + ts.getAbbrMonth() + ' ' + ts.getFullYear();
   };
 
