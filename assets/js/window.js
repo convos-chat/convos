f73436bcecf9af39f26a99b4fbc28a7cd381622f
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-02-26 23:50:30 +0100

    Add String.prototype.parseUrl()

--- assets/js/window.js
+++ assets/js/window.js
@@ -43,6 +43,18 @@ window.isTouchDevice = !!('ontouchstart' in window);
 window.isWideScreen = window.innerWidth > wideScreenWidth;
 window.TODO = function(message) { alert("TODO: " + message); };
 
+String.prototype.parseUrl = function() {
+  var m = this.match(new RegExp("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?"));
+  if (!m) return {};
+  var s = {scheme: m[2], authority: m[4], path: m[5], fragment: m[9], query: {}};
+  var p = m[4].split('@', 2);
+  s.hostPort = p[1] || p[0];
+  s.userinfo = p.length == 2 ? p[0].split(':', 2);
+  p = m[7] ? m[7].split('&') : [];
+  p.forEach(function(i) { var kv = i.split('=', 2); s.query[kv[0]] = kv[1]; });
+  return s;
+};
+
 String.prototype.ucFirst = function() {
   return this.replace(/^./, function (match) {
     return match.toUpperCase();
