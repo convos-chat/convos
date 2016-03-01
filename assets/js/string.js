d71b57928df771a9236dd403e6a6daa2cafb1594
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-03-01 21:27:02 +0100

    Add String.prototype.mdToHtml(), closes #254

--- assets/js/string.js
+++ assets/js/string.js
@@ -1,4 +1,21 @@
-function() {
+(function() {
+  // *foo*     or _foo_     = <em>foo</em>
+  // **foo**   or __foo__   = <strong>foo</strong>
+  // ***foo*** or ___foo___ = <em><strong>foo</strong></em>
+  // \*foo*    or \_foo_    = *foo* or _foo_
+  var mdToHtmlRe = new RegExp('(\\\\?)(\\*+|_+)(\\w.*?)\\2', 'g');
+  String.prototype.mdToHtml = function() {
+    return this.replace(mdToHtmlRe, function(all, esc, tag, text) {
+      switch (tag.length) {
+        case 1: return esc ? all.replace(/^\\/, '') : '<em>' + text + '</em>';
+        case 2: return esc ? all.replace(/^\\/, '') : '<strong>' + text + '</strong>';
+        case 3: return esc ? all.replace(/^\\/, '') : '<em><strong>' + text + '</strong></em>';
+        default: return all;
+      }
+    });
+  };
+
+  var urlRe = new RegExp("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?");
   String.prototype.parseUrl = function() {
     var m = this.match(urlRe);
     if (!m) return {};
