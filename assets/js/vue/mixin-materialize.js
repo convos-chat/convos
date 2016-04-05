e72650bfe2074b9604cb48ba3cc6c6b238d30ee9
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-04-05 22:37:01 +0200

    Add Materialize.updateTextFields

--- assets/js/vue/mixin-materialize.js
+++ assets/js/vue/mixin-materialize.js
@@ -2,14 +2,17 @@
 Vue.mixin({
   methods: {
     materializeComponent: function() {
-      $('.material-tooltip').hide();
-      $('.tooltipped[title]', this.$el).each(function() {
+      $(".material-tooltip").hide();
+      $(".tooltipped[title]", this.$el).each(function() {
         var $t = $(this);
-        $t.attr('data-tooltip', $t.attr('title') || $t.attr('placeholder')).removeAttr('title');
-      }).filter('[data-tooltip]').tooltip();
+        $t.attr("data-tooltip", $t.attr("title") || $t.attr("placeholder")).removeAttr("title");
+      }).filter("[data-tooltip]").tooltip();
+      this.$nextTick(function() {
+        Materialize.updateTextFields();
+      });
     }
   },
   created: function() {
     this.materializeComponent();
   }
-})
+});
