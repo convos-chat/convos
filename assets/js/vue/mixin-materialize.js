9217d2e0c517aab87fb63b7e8c05131b0d6167d5
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-04-14 21:25:16 +0200

    Improved handling of tooltips

--- assets/js/vue/mixin-materialize.js
+++ assets/js/vue/mixin-materialize.js
@@ -1,19 +1,67 @@
 // Global mixin for rendering materialize components
-Vue.mixin({
-  methods: {
-    materializeComponent: function() {
-      $(".material-tooltip").hide();
-      $(".tooltipped", this.$el).each(function() {
-        var $t = $(this);
-        if ($t.attr("data-tooltip")) return;
-        $t.attr("data-tooltip", $t.attr("title") || $t.attr("placeholder")).removeAttr("title");
-      }).filter("[data-tooltip]").tooltip();
-      this.$nextTick(function() {
-        Materialize.updateTextFields();
-      });
+(function() {
+  var $tooltip = '<div class="material-tooltip"><span></span></div>';
+  var spacing  = 4;
+
+  var showTooltip = function() {
+    if (typeof $tooltip == "string") {
+      $tooltip                 = $($tooltip).appendTo("body");
+      $tooltip.transitionDelay = $tooltip.css("transition-delay").replace(/s/, "");
     }
-  },
-  created: function() {
-    this.materializeComponent();
-  }
-});
+
+    var $el    = $(this);
+    var offset = $el.offset();
+    var css    = {};
+
+    $tooltip.text($el.attr("data-hint")).addClass("active");
+
+    css.right = "auto";
+    css.left  = offset.left - ($tooltip.outerWidth() - $el.outerWidth()) / 2;
+    css.top   = offset.top - $tooltip.outerHeight() - spacing;
+
+    if (css.left < 0) {
+      css.left = spacing;
+    } else if (css.left + $tooltip.outerWidth() + spacing > $(window).width()) {
+      css.left  = "unset";
+      css.right = spacing;
+    }
+
+    if (css.top < spacing) {
+      css.top = offset.top + $el.outerHeight() + spacing;
+    }
+
+    Object.keys(css).forEach(function(k) {
+      if (parseInt(css[k]))
+        css[k] = css[k] + "px";
+    });
+
+    $tooltip.css(css);
+  };
+
+  var hideToolTip = function() {
+    $tooltip.css("transition-delay", "100ms").removeClass("active");
+    setTimeout(function() {
+      $tooltip.css("transition-delay", $tooltip.transitionDelay + "s");
+    }, $tooltip.transitionDelay * 1000);
+  };
+
+  Vue.mixin({
+    methods: {
+      materializeComponent: function() {
+        this.$nextTick(function() {
+          this.overrideTooltips();
+          Materialize.updateTextFields();
+        });
+      },
+      overrideTooltips: function() {
+        $("[data-hint]", this.$el).each(function() {
+          var $el = $(this);
+          if (!$el.hasClass("tooltipped")) $el.hover(showTooltip, hideToolTip).addClass("tooltipped");
+        });
+      }
+    },
+    created: function() {
+      this.materializeComponent();
+    }
+  });
+})();
