7c3de7b9228c1643b669668e57ab1a1f48a104e0
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-08-13 13:45:09 +0200

    <md-autocomplete> is not wrapped in a <div>

--- assets/vue/md-autocomplete.vue
+++ assets/vue/md-autocomplete.vue
@@ -1,23 +1,22 @@
 <template>
-  <div class="md-autocomplete">
-    <input
-      :id="id"
-      v-model="value"
-      @keydown="keydown"
-      @keyup="keyup"
-      type="text" autocomplete="off" spellcheck="false">
-    <div class="autocomplete">
-      <ul>
-        <li :class="optionClass(o, $index)" v-for="o in filteredOptions" :data-hint="o.title">
-          <a href="option://{{o.value}}" class="truncate" @click="select(o)">{{o.text || o.value}}</a>
-        </li>
-      </ul>
-    </div>
+  <input
+    :id="id"
+    :placeholder="placeholder"
+    v-model="value"
+    @keydown="keydown"
+    @keyup="keyup"
+    type="text" autocomplete="off" spellcheck="false">
+  <div class="autocomplete" :class="filteredOptions.length ? '' : 'hidden'">
+    <ul>
+      <li :class="optionClass(o, $index)" v-for="o in filteredOptions" :data-hint="o.title">
+        <a href="option://{{o.value}}" class="truncate" @click="select(o)">{{o.text || o.value}}</a>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
 module.exports = {
-  props: ["id", "value", "options"],
+  props: ["id", "placeholder", "options", "value"],
   computed: {
     filteredOptions: function() {
       var re = new RegExp(this.value, 'i');
