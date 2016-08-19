692f45cd7be377037a8bbcc3cc5a46e81db8df03
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-08-19 21:20:47 +0200

    Force breaking long words inside a message template

--- assets/vue/convos-message-notice.vue
+++ assets/vue/convos-message-notice.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="convos-message notice" :class="classNames()">
     <a href="#{{msg.from}}" class="title" @click.prevent="insertIntoInput">{{msg.from}}</a>
-    <div class="message"><span>{{{message() | markdown}}}</span></div>
+    <div class="message">{{{message() | markdown}}}</div>
     <span class="secondary-content ts" v-tooltip="msg.ts.toLocaleString()">{{msg.ts | timestring}}</span>
   </div>
 </template>
