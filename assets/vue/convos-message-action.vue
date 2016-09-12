255896cf054a8b4ee564d0ef99b18018cf2ef30d
Marcus Ramberg <marcus.ramberg@usit.uio.no>
2016-09-12 22:32:52 +0200

    Use black four pointed star for /me actions

--- assets/vue/convos-message-action.vue
+++ assets/vue/convos-message-action.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="convos-message action" :class="classNames()">
-    <span>&star;</span>
+    <span>&#10022;</span>
     <span class="secondary-content ts" v-tooltip="msg.ts.toLocaleString()">{{msg.ts | timestring}}</span>
     <a href="#{{msg.from}}" class="title" @click.prevent="insertIntoInput">{{msg.from}}</a>
     <div class="message">{{{message() | markdown}}}</div>
