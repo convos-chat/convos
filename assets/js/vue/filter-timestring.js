04f2381cef81cb08f91f474e61f947a8a1e42212
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-09-16 09:23:24 +0200

    Fix "whois" for participant who is not in any channels

--- assets/js/vue/filter-timestring.js
+++ assets/js/vue/filter-timestring.js
@@ -19,4 +19,30 @@
 
     return ts.getDate() + ". " + ts.getAbbrMonth() + " " + ts.getFullYear();
   });
+
+  // 1h 4m 2s
+  Vue.filter("seconds", function(s) {
+    var x, str = [];
+    if (!s) return "0s";
+    if (typeof s == "string" && s.match(/^[a-z]/i)) return s; // In case of "Not loaded" as input
+    if (s > ONE_DAY) {
+      x = parseInt(s / ONE_DAY);
+      s -= x * ONE_DAY;
+      str.push(x + "d");
+    }
+    if (s > ONE_HOUR) {
+      x = parseInt(s / ONE_HOUR);
+      s -= x * ONE_HOUR;
+      str.push(x + "h");
+    }
+    if (s > ONE_MINUTE) {
+      x = parseInt(s / ONE_MINUTE);
+      s -= x * ONE_MINUTE;
+      str.push(x + "m");
+    }
+    if (str.length < 3 && (!str.length || s)) {
+      str.push(s + "s");
+    }
+    return str.join(" ");
+  });
 })();
--- assets/vue/convos-message-whois.vue
+++ assets/vue/convos-message-whois.vue
@@ -3,13 +3,14 @@
     <span class="secondary-content ts" v-tooltip="msg.ts.toLocaleString()">{{msg.ts | timestring}}</span>
     <a href="#chat" class="title">{{msg.from}}</a>
     <div class="message">
-      <a href="#chat:{{msg.nick}}" @click.prevent="send('/query ' + msg.nick)">{{msg.nick}}</a>
-      <template v-if="msg.idle_for">has been idle for {{msg.idle_for}} seconds in</template>
-      <template v-else="msg.idle_for">is active in</template>
+      <a href="#chat:{{msg.nick}}" @click.prevent="send('/query ' + msg.nick)" v-tooltip="msg.name || 'No name?'">{{msg.nick}}</a>
+      (<abbr v-tooltip="msg.server + ' - ' + msg.server_info">{{msg.user.replace(/^\~/, '')}}@{{msg.host}}</abbr>)
+      <template v-if="msg.idle_for">has been idle for {{msg.idle_for | seconds}}{{channels.length ? " in" : "."}}</template>
+      <template v-if="!msg.idle_for && channels.length">is active in</template>
+      <template v-if="!msg.idle_for && !channels.length">is not active in any channels.</template>
       <template v-for="name in channels">
-        {{!$index ? '' : $index + 1 == channels.length ? "and" : ", "}}
-        <a href="#join:{{name}}" @click.prevent="send('/join ' + name)">{{name}}</a>
-      </template>.
+        <a href="#join:{{name}}" @click.prevent="send('/join ' + name)">{{name}}</a>{{$index + 2 == channels.length ? " and " : $index + 1 == channels.length ? "." : ", "}}
+      </template>
     </div>
   </div>
 </template>
