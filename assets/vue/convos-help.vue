5fd44acd4c87d27707e90d33e55fadb7d059da48
Marcus Ramberg <marcus.ramberg@gmail.com>
2016-11-16 14:33:43 +0100

    Remove obsolete help msgs

--- assets/vue/convos-help.vue
+++ assets/vue/convos-help.vue
@@ -8,7 +8,6 @@
       <div class="row">
         <div class="col s12">
           <h5 v-if="!isTouchDevice">Shortcuts</h5>
-          <p v-if="!isTouchDevice">These shortcuts are not yet implemented.</p>
           <dl v-if="!isTouchDevice">
             <dt>shift+enter</dt>
             <dd>Shift focus between chat input and dialog sidebar.</dd>
@@ -16,7 +15,6 @@
             <dd>Will autocomplete a command, nick or channel name.</dd>
           </dl>
           <h5>Available commands</h5>
-          <p>Commands are not yet supported.</p>
           <dl>
             <template v-for="cmd in commands">
               <dt><a @click.prevent="insertIntoInput" href="#/{{cmd.command}}">{{cmd.example || "/" + cmd.command}}</a></dt>
