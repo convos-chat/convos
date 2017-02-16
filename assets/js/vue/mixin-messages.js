7ef3567e326868db6943f28e0fd731923e4fadcd
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-02-16 10:01:10 +0100

    Emulating scroll event

--- assets/js/vue/mixin-messages.js
+++ assets/js/vue/mixin-messages.js
@@ -4,58 +4,48 @@
   Convos.mixin.messages = {
     watch: {
       "dialog.active": function(v, o) {
-        if (v === false && this.scrollPosTid) clearTimeout(this.scrollPosTid);
-        if (v === true) this.scrollPosTid = setInterval(this.keepScrollPos, 300);
+        if (v === false && this._trackTid) clearTimeout(this.trackViewPort);
+        if (v === true) this._trackTid = setInterval(this.trackViewPort, 200);
       }
     },
     methods: {
-      onScroll: function(e) {
-        if (DEBUG.scroll) console.log("[onScroll:" + this.dialog.dialog_id + "]", this.atBottom, this.scrollEl.scrollTop, this.scrollEl.scrollHeight - this.scrollEl.offsetHeight, this.scrollTid);
-        if (this.scrollTid == "skip") return this.scrollTid = undefined;
-        if (!this.scrollTid) this.scrollTid = setTimeout(this.onScrollDelayed, 150);
-      },
-      onScrollDelayed: function() {
+      trackViewPort: function() {
         var self = this;
         var el = this.scrollEl;
-        var messages = this.$refs.messages;
-        var innerHeight = window.innerHeight || document.documentElement.clientHeight;
         var scrollHeight = el.scrollHeight;
         var scrollTop = el.scrollTop;
+        var diff = {scrollHeight: scrollHeight - this.scrollHeight, scrollTop: scrollTop - this.scrollTop};
 
-        this.atBottom = scrollHeight - el.offsetHeight < scrollTop + THRESHOLD;
+        if (!diff.scrollHeight && !diff.scrollTop) return;
+        if (!diff.scrollHeight) this.atBottom = scrollHeight - el.offsetHeight < scrollTop + THRESHOLD;
+        if (DEBUG.scroll) console.log(["[scroll:" + this.dialog.dialog_id + "]", this.atBottom, this.$refs.messages.length, el.scrollTop + "-" + this.scrollTop, el.scrollHeight + "-" + this.scrollHeight].join(" "));
+        if (this.atBottom) el.scrollTop = scrollHeight;
 
+        this.scrollHeight = scrollHeight;
+        this.scrollTop = scrollTop;
+
+        var messages = this.$refs.messages;
+        var breakTop = this.scrollTop + (window.innerHeight || document.documentElement.clientHeight);
         for (var i = 0; i < messages.length; i++) {
           var offsetTop = messages[i].$el.offsetTop;
-          if (offsetTop > scrollTop + innerHeight) break;
+          if (offsetTop > breakTop) break;
           if (offsetTop < scrollTop) continue;
           messages[i].$emit("visible");
         }
 
-        if (scrollTop > THRESHOLD) {
-          return this.scrollTid = undefined;
-        }
-        else {
+        if (diff.scrollTop && scrollTop < THRESHOLD) {
           this.dialog.load({historic: true}, function(err, body) {
-            self.$nextTick(function() {
-              self.scrollTid = "skip";
-              el.scrollTop = scrollTop + el.scrollHeight - scrollHeight;
-            });
+            self.$nextTick(function() { el.scrollTop = scrollTop + el.scrollHeight - scrollHeight });
           });
         }
-      },
-      keepScrollPos: function() {
-        if (DEBUG.keepScrollPos) console.log("[keepScroll:" + this.dialog.dialog_id + "]", this.atBottom, this.scrollEl.scrollTop, this.scrollEl.scrollHeight - this.scrollEl.offsetHeight, this.scrollTid);
-        if (this.atBottom) this.scrollEl.scrollTop = this.scrollEl.scrollHeight - this.scrollEl.offsetHeight;
       }
     },
     ready: function() {
       this.atBottom = true;
+      this.scrollHeight = 0;
+      this.scrollTop = 0;
       this.scrollEl = this.$el.querySelector(".scroll-element");
-      this.scrollEl.addEventListener("scroll", this.onScroll);
-      this.dialog.on("message", function() { this.$nextTick(this.keepScrollPos); }.bind(this));
-    },
-    beforeDestroy: function() {
-      this.scrollEl.removeEventListener("scroll", this.onScroll);
+      this.dialog.on("message", function() { this.$nextTick(this.trackViewPort); }.bind(this));
     }
   };
 })();
