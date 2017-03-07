6ed7cb074e616eb14b61cca4d22693060d500811
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-03-07 21:18:54 +0100

    Refactor <convos-login/> and -register. Add <convos-recover/>

---
+++ assets/vue/convos-recover.vue
@@ -0,0 +1,27 @@
+<template>
+  <convos-not-logged-in screen="recover" :user="user">
+    <div class="row">
+      <md-input :value.sync="email" type="email" id="form_login_email">Your login email</md-input>
+    </div>
+    <div class="row">
+      <div class="col s12">
+        Forgot your password? Tell your Convos admin to run the command below,
+        and then send you the recovery link:
+        <pre class="stylish">$ convos get /user/recover/{{email || 'your-email@example.com'}}</pre>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col s12">
+        <a :href="'#login" @click.prevent="user.currentPage = 'convos-recover'" class="btn waves-effect waves-light">Login</a>
+      </div>
+    </div>
+  </convos-not-logged-in>
+</template>
+<script>
+module.exports = {
+  props: ["user"],
+  data: function() {
+    return {email: localStorage.getItem("email") || ""};
+  }
+};
+</script>
--- assets/vue/convos-register.vue
+++ assets/vue/convos-register.vue
@@ -1,55 +1,29 @@
 <template>
-  <div class="convos-register">
-    <div class="row not-logged-in-wrapper">
-      <form @submit.prevent="register" class="col s12 m6 offset-m3">
-        <div class="row">
-          <div class="col s12">
-            <h1>Convos</h1>
-            <p><i>- Collaboration done right.</i></p>
-          </div>
-        </div>
-        <div class="row">
-          <md-input :value.sync="email" :focus="true" id="form_login_email">Email</md-input>
-        </div>
-        <div class="row">
-          <md-input :value.sync="password" cols="s6" type="password" id="form_login_password">Password</md-input>
-          <md-input :value.sync="passwordAgain" cols="s6" type="password" placeholder="Repeat password" id="form_login_password_again"></md-input>
-        </div>
-        <div class="row" v-if="settings.invite_code">
-          <md-input :value.sync="invite_code" type="password" id="form_login_invite_code">Invite code</md-input>
-        </div>
-        <div class="row" v-if="errors.length">
-          <div class="col s12"><div class="alert">{{errors[0].message}}</div></div>
-        </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <button class="btn waves-effect waves-light" type="submit">Register</button>
-            <a href="#login" @click.prevent="user.currentPage = 'convos-login'" class="btn-flat waves-effect waves-light">Log in</a>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col s12 about">
-            <template v-if="settings.organization_url != 'http://convos.by'">
-              <a :href="settings.organization_url">{{settings.organization_name}}</a> -
-            </template>
-            <a href="http://convos.by">About</a> -
-            <a href="http://convos.by/doc">Documentation</a> -
-            <a href="http://convos.by/blog">Blog</a>
-          </div>
-        </div>
-      </form>
+  <convos-not-logged-in screen="register" :user="user" @submit="register">
+    <div class="row">
+      <md-input :value.sync="email" :focus="true" id="form_login_email">Email</md-input>
     </div>
-  </div>
+    <div class="row">
+      <md-input :value.sync="password" cols="s6" type="password" id="form_login_password">Password</md-input>
+      <md-input :value.sync="passwordAgain" cols="s6" type="password" placeholder="Repeat password" id="form_login_password_again"></md-input>
+    </div>
+    <div class="row" v-if="settings.invite_code">
+      <md-input :value.sync="invite_code" type="password" id="form_login_invite_code">Invite code</md-input>
+    </div>
+    <div class="row" v-if="errors.length">
+      <div class="col s12"><div class="alert">{{errors[0].message}}</div></div>
+    </div>
+  </convos-not-logged-in>
 </template>
 <script>
 module.exports = {
-  props:    ["user"],
-  data:     function() {
+  props: ["user"],
+  data: function() {
     return {
-      invite_code:   "",
-      email:         localStorage.getItem("email") || "",
-      errors:        [],
-      password:      "",
+      invite_code: "",
+      email: localStorage.getItem("email") || "",
+      errors: [],
+      password: "",
       passwordAgain: "",
     };
   },
