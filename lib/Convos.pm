4abb6d0ce1da5bc7a4a1a2d4f5b48a7e2d679afa
Jan Henning Thorsen <jhthorsen@cpan.org>
2017-04-13 13:50:44 +0200

    Released 0.99.30

--- lib/Convos.pm
+++ lib/Convos.pm
@@ -11,7 +11,7 @@ use Mojo::Util;
 
 $ENV{CONVOS_PLUGINS} //= 'Convos::Plugin::Paste';
 
-our $VERSION = '0.99_29';
+our $VERSION = '0.99_30';
 
 my $ANON_API_CONTROLLER = "Convos::Controller::Anon";
 
@@ -237,7 +237,7 @@ Convos - Multiuser chat application
 
 =head1 VERSION
 
-0.99_29
+0.99_30
 
 =head1 DESCRIPTION
 
