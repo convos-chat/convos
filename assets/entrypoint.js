d37fa768620fd1fdb6a73389a38ea876d1274980
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-05-25 14:31:22 +0200

    Fix sw.js.ep after changing to M::P::Webpack

--- assets/entrypoint.js
+++ assets/entrypoint.js
@@ -1,7 +1,20 @@
 import './sass/convos.scss';
 import App from './App.svelte';
 
+console.log('[Convos] Initializing application.');
+
+const body = document.querySelector('body');
 const removeEls = document.querySelectorAll('.js-remove');
 const app = new App({target: document.body});
 
+body.classList.remove('no-js');
 for (let i = 0; i < removeEls.length; i++) removeEls[i].remove();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', e => {
+    navigator.serviceWorker.register('/sw.js').then(
+      (reg) => window.updateServiceWorker && reg.update(),
+      (err) => console.log('[Convos] ServiceWorker registration failed:', err),
+    );
+  });
+}
