71ef072447f804a1212ce85a0543f21946046435
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-05-27 23:17:46 +0200

    Have an idea about rendering notifications on /chat

--- assets/page/Chat.svelte
+++ assets/page/Chat.svelte
@@ -2,7 +2,7 @@
 import {getContext, tick} from 'svelte';
 import {l} from '../js/i18n';
 import {md} from '../js/md';
-import {connections, dialogs} from '../store/user';
+import {connections, dialogs, notifications, unread} from '../store/user';
 import {pathParts} from '../store/router';
 import DialogSettings from '../components/DialogSettings.svelte';
 import DialogSubject from '../components/DialogSubject.svelte';
@@ -19,7 +19,10 @@ let height = 0;
 let messages = [];
 let scrollDirection = 'down'; // TODO: Change it to up, when scrolling up
 let settingsIsVisible = false;
-let subject = ''; // TODO: Get it from Api
+
+function clearNotifications(e) {
+  alert('TODO');
+}
 
 function isSame(i) {
   return i == 0 ? false : messages[i].from == messages[i - 1].from;
@@ -32,7 +35,7 @@ function toggleSettings(e) {
 pathParts.subscribe(async ($pathParts) => {
   messages = [];
   settingsIsVisible = false;
-  if (!$pathParts[1]) return;
+  if (!$pathParts[1]) return (messages = $notifications);
   const operationId = $pathParts[2] ? 'dialogMessages' : 'connectionMessages';
   const res = await api.execute(operationId, {connection_id: $pathParts[1], dialog_id: $pathParts[2]});
   messages = res.messages || [];
@@ -50,11 +53,19 @@ $: settingComponent = $pathParts[2] ? DialogSettings : ServerSettings;
 
 <main class="main-app-pane" class:has-visible-settings="{settingsIsVisible}" bind:offsetHeight="{height}">
   <h1 class="main-header">
-    <span>{$pathParts[2] || $pathParts[1] || l('TODO')}</span>
-    <small><DialogSubject obj="{dialog.dialog_id ? dialog : connection}"/></small>
-    <a href="#settings" class="main-header_settings-toggle" on:click|preventDefault="{toggleSettings}"><Icon name="sliders-h"/></a>
-    <StateIcon obj="{dialog.dialog_id ? dialog : connection}"/>
+    <span>{$pathParts[2] || $pathParts[1] || l('Notifications')}</span>
+    {#if connection.connection_id}
+      <small><DialogSubject obj="{dialog.dialog_id ? dialog : connection}"/></small>
+      <a href="#settings" class="main-header_settings-toggle" on:click|preventDefault="{toggleSettings}"><Icon name="sliders-h"/></a>
+      <StateIcon obj="{dialog.dialog_id ? dialog : connection}"/>
+    {:else}
+      <a href="#clear" class="main-header_settings-toggle" on:click|preventDefault="{clearNotifications}"><Icon name="{$unread ? 'bell' : 'bell-slash'}"/></a>
+    {/if}
   </h1>
+
+  {#if messages.length == 0}
+    <h2>{l(connection.connection_id ? 'No messages.' : 'No notifications.')}</h2>
+  {/if}
 
   {#each messages as message, i}
     <div class="message" class:is-same="{isSame(i)}" class:is-hightlight="{message.highlight}">
