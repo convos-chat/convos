991b3014eb9126d5c63e88a52924b3e0d7d07133
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-06-22 14:49:49 +0200

    Add support for variables in l()

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,9 +1,13 @@
 import {md} from './md';
 
-export function l(str, params) {
-  return str; // TODO
+export const dict = {en: {}};
+export let lang = 'en';
+
+export function l(lexicon, ...vars) {
+  const translated = String(dict[lexicon] && dict[lexicon][lang] || lexicon);
+  return translated.replace(/%(\d+)/g, (a, i) => { return vars[i - 1] });
 }
 
 export function lmd(str, params) {
-  return md(l(str)); // TODO
+  return md(l(str));
 }
