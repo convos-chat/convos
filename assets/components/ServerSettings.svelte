255899c033723051c32ba7e73056e1077f3d4f50
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-09-03 13:33:01 +0900

    Moved dialog and server settings into the main menu

--- assets/components/ServerSettings.svelte
+++ assets/components/ServerSettings.svelte
@@ -1,5 +1,5 @@
 <script>
-import {getContext, onMount, tick} from 'svelte';
+import {getContext, tick} from 'svelte';
 import {l} from '../js/i18n';
 import Button from '../components/Button.svelte';
 import Checkbox from '../components/form/Checkbox.svelte';
@@ -7,6 +7,7 @@ import ConnURL from '../js/ConnURL';
 import FormActions from '../components/form/FormActions.svelte';
 import OperationStatus from '../components/OperationStatus.svelte';
 import PasswordField from '../components/form/PasswordField.svelte';
+import SettingsHeader from '../components/SettingsHeader.svelte';
 import TextField from '../components/form/TextField.svelte';
 
 export let dialog = {};
@@ -33,10 +34,10 @@ async function updateConnectionFromForm(e) {
 }
 
 $: connection = $user.findDialog({connection_id: dialog.connection_id}) || {};
+$: connectionHost = connection.url && connection.url.host;
 $: isNotDisconnected = connection.state != 'disconnected';
 
-onMount(async () => {
-  if (!formEl) await tick();
+$: if (connection.url && formEl) {
   formEl.server.value = connection.url.host;
   formEl.nick.value = connection.url.searchParams.get('nick') || '';
   formEl.password.value = connection.url.password;
@@ -45,42 +46,47 @@ onMount(async () => {
   useTls = connection.url.searchParams.get('tls') && true || false;
   verifyTls = connection.url.searchParams.get('tls_verify') && true || false;
   wantToBeConnected = connection.wanted_state == 'connected';
-  console.log('A', useTls);
-});
+}
 </script>
 
-<form method="post" bind:this="{formEl}" on:submit|preventDefault="{updateConnectionFromForm}">
-  <input type="hidden" name="connection_id" value="{connection.connection_id}">
-  <input type="hidden" name="url" value="{url}">
-  <TextField name="server" placeholder="{l('Ex: chat.freenode.net:6697')}">
-    <span slot="label">{l('Server and port')}</span>
-  </TextField>
-  <TextField name="nick" placeholder="{l('Ex: your-name')}">
-    <span slot="label">{l('Nickname')}</span>
-  </TextField>
-  <Checkbox bind:checked="{wantToBeConnected}">
-    <span slot="label">{l('Want to be connected')} ({l('Currently %1', connection.state || 'disconnected')})</span>
-  </Checkbox>
-  <Checkbox name="tls" bind:checked="{useTls}">
-    <span slot="label">{l('Secure connection (TLS)')}</span>
-  </Checkbox>
-  {#if useTls}
-    <Checkbox name="tls_verify" bind:checked="{verifyTls}">
-      <span slot="label">{l('Verify certificate (TLS)')}</span>
+<div class="sidebar-wrapper is-visible">
+  <SettingsHeader {dialog}/>
+
+  <p>{l('Currently %1 from %2.', connection.state || 'disconnected', connectionHost)}</p>
+
+  <form method="post" bind:this="{formEl}" on:submit|preventDefault="{updateConnectionFromForm}">
+    <input type="hidden" name="connection_id" value="{connection.connection_id}">
+    <input type="hidden" name="url" value="{url}">
+    <TextField name="server" placeholder="{l('Ex: chat.freenode.net:6697')}">
+      <span slot="label">{l('Server and port')}</span>
+    </TextField>
+    <TextField name="nick" placeholder="{l('Ex: your-name')}">
+      <span slot="label">{l('Nickname')}</span>
+    </TextField>
+    <Checkbox bind:checked="{wantToBeConnected}">
+      <span slot="label">{l('Want to be connected')}</span>
     </Checkbox>
-  {/if}
-  <Checkbox bind:checked="{showAdvancedSettings}">
-    <span slot="label">{l('Show advanced settings')}</span>
-  </Checkbox>
-  <TextField name="username" hidden="{!showAdvancedSettings}">
-    <span slot="label">{l('Username')}</span>
-  </TextField>
-  <PasswordField name="password" hidden="{!showAdvancedSettings}">
-    <span slot="label">{l('Password')}</span>
-  </PasswordField>
-  <FormActions>
-    <Button icon="save">{l('Update')}</Button>
-    <Button icon="trash" on:click|preventDefault="{deleteConnection}">{l('Delete')}</Button>
-  </FormActions>
-  <OperationStatus op={updateConnectionOp}/>
-</form>
+    <Checkbox name="tls" bind:checked="{useTls}">
+      <span slot="label">{l('Secure connection (TLS)')}</span>
+    </Checkbox>
+    {#if useTls}
+      <Checkbox name="tls_verify" bind:checked="{verifyTls}">
+        <span slot="label">{l('Verify certificate (TLS)')}</span>
+      </Checkbox>
+    {/if}
+    <Checkbox bind:checked="{showAdvancedSettings}">
+      <span slot="label">{l('Show advanced settings')}</span>
+    </Checkbox>
+    <TextField name="username" hidden="{!showAdvancedSettings}">
+      <span slot="label">{l('Username')}</span>
+    </TextField>
+    <PasswordField name="password" hidden="{!showAdvancedSettings}">
+      <span slot="label">{l('Password')}</span>
+    </PasswordField>
+    <FormActions>
+      <Button icon="save">{l('Update')}</Button>
+      <Button icon="trash" on:click|preventDefault="{deleteConnection}">{l('Delete')}</Button>
+    </FormActions>
+    <OperationStatus op={updateConnectionOp}/>
+  </form>
+</div>
---
+++ assets/components/SettingsHeader.svelte
@@ -0,0 +1,17 @@
+<script>
+import {l} from '../js/i18n';
+import {onMount} from 'svelte';
+import Icon from '../components/Icon.svelte';
+import Link from '../components/Link.svelte';
+
+export let dialog = {};
+
+let linkEl;
+
+onMount(() => linkEl.focus());
+</script>
+
+<Link href="/chat/{dialog.path}" className="sidebar__header for-settings" bind:this="{linkEl}">
+  <h2>{l(dialog.dialog_id ? 'Conversation' : 'Server')}</h2>
+  <Icon name="times-circle"/>
+</Link>
