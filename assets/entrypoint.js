74323319a5780c19a71e5f19d026e4a090270032
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-10-16 15:05:38 +0900

    Less flickering on the loading screen

--- assets/entrypoint.js
+++ assets/entrypoint.js
@@ -5,7 +5,7 @@ console.log('[Convos] Initializing application.');
 
 const body = document.querySelector('body');
 const app = new App({target: document.body});
-body.classList.remove('no-js');
+body.classList = body.className.replace(/no-js/, 'has-js');
 window.renderGrid = () => body.classList.add('with-grid');
 
 if ('serviceWorker' in navigator) {
