e4371cea8a81b3f6067684151248eeaeaf068b03
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-10-22 16:03:45 +0900

    Fix restoring attributes from localStorage

--- assets/store/User.js
+++ assets/store/User.js
@@ -18,8 +18,8 @@ export default class User extends Reactive {
     this._readOnlyAttr('getUserOp', api.operation('getUser', {connections: true, dialogs: true, notifications: true}));
     this._readOnlyAttr('notifications', new Notifications({api, events: this.events, messagesOp: this.getUserOp}));
 
-    this._updateableAttr('icon', 'user-circle');
-    this._updateableAttr('lastUrl', '');
+    this._localStorageAttr('icon', 'user-circle');
+    this._localStorageAttr('lastUrl', '');
 
     this._proxyAttr('expandUrlToMedia', 'embedMaker');
     this._proxyAttr('wantNotifications', 'events');
