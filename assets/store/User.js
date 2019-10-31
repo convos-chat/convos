172e101ebc177fe2e5da2cd771af2adf9c07cc82
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-10-31 13:02:21 +0900

    Fix hljs for dark mode #313

--- assets/store/User.js
+++ assets/store/User.js
@@ -37,7 +37,7 @@ export default class User extends Reactive {
       conn.dialogs.toArray().forEach(dialog => {
         if (!cb || cb(dialog)) dialogs.push(dialog);
       });
-    })
+    });
 
     return dialogs;
   }
