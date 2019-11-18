8bbc835c849cccb3e8823edcd32cb9282f30134e
Jan Henning Thorsen <jhthorsen@cpan.org>
2019-11-18 09:58:31 +0900

    Operations.perform(formEl) can handle checkboxes

--- assets/store/Operation.js
+++ assets/store/Operation.js
@@ -138,10 +138,15 @@ export default class Operation extends Reactive {
   }
 
   _extractValue(params, p) {
-    if (p.schema && (params.tagName || '').toLowerCase() == 'form' ) {
+    if (p.schema && (params.tagName || '').toLowerCase() == 'form') {
       const body = {};
-      Object.keys(p.schema.properties).forEach(k => { body[k] = params[k] && params[k].value });
-      return body;
+      return Object.keys(p.schema.properties).reduce((body, k) => {
+        const inputEl = params[k];
+        if (!inputEl) return body; // No such form element
+        if (inputEl.type == 'checkbox' && !inputEl.checked) return body;
+        body[k] = inputEl.value;
+        return body;
+      }, {});
     }
     else if (params[p.name] && params[p.name].tagName) {
       return params[p.name].value;
