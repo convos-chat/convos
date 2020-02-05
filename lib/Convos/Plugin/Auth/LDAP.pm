170b6f6ab900e58d5a9fce4a8ae8568c915e1ad5
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-02-05 21:09:51 +0900

    Add OAuth2 support to Convos, closes #414

--- lib/Convos/Plugin/Auth/LDAP.pm
+++ lib/Convos/Plugin/Auth/LDAP.pm
@@ -21,7 +21,7 @@ sub register {
   require_module('Net::LDAP');
 
   $app->helper('auth.login_p' => sub { $self->_login_p(@_) });
-  $app->log->debug("Loaded Convos::Plugin::Auth $ldap_url");
+  $app->log->debug("Loaded Convos::Plugin::Auth::LDAP $ldap_url");
 }
 
 sub _bind_params {
@@ -110,7 +110,7 @@ Convos::Plugin::Auth::LDAP - Convos plugin for logging in users from LDAP
 
 =head1 DESCRIPTION
 
-L<Convos::Plugin::Auth::LDAP> allows convos to register and login users from
+L<Convos::Plugin::Auth::LDAP> allows Convos to register and login users from
 an LDAP database.
 
 =head1 ENVIRONMENT VARIABLES
