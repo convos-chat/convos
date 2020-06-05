d5f29e960c4c2f7b5cb0177c706f625a7c8ed6c2
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-06-05 11:49:50 +0900

    Fix <SelectField> click and filter issues

--- assets/components/form/SelectField.svelte
+++ assets/components/form/SelectField.svelte
@@ -11,18 +11,18 @@ let humanEl;
 let open = false;
 let typed = '';
 let wrapperEl;
+let visibleOptions = [];
 
 export let hidden = false;
 export let name = '';
 export let id = name ? 'form_' + name : uuidv4();
-export let options = [];
+export let options = []; // [["value", "label"], ...]
 export let placeholder = '';
 export let readonly = false;
 export let value = '';
 
-$: visibleOptions = filterOptions(options, typed);
+$: calculateVisibleOptions(options, typed);
 $: if (humanEl) renderHuman(value);
-$: if (open == true) activeIndex = 0;
 $: if (open == false) typed = '';
 
 $: if (hiddenEl && !hiddenEl.syncValue) {
@@ -36,24 +36,32 @@ $: if (humanEl && !humanEl.value && humanEl != document.activeElement) {
 }
 
 onMount(() => {
-  humanEl.addEventListener('blur', () => setTimeout(() => { open = false }, 100));
   value = hiddenEl.value;
+  humanEl.addEventListener('blur', () => {
+    setTimeout(() => document.activeElement.closest('.select-field__option') || (open = false), 100);
+    renderHuman(value);
+  });
 });
 
-function filterOptions(options, needle) {
+function calculateVisibleOptions(options, needle) {
   let re = new RegExp('(?:^|\\s|-)' + regexpEscape(needle), 'i'); // TODO: needle need to be safe string
-  let found = [];
-  if (!found.length) found = options.filter(opt => opt[0] == needle);
+
+  // Match by value exact
+  let found = options.filter(opt => opt[0] == needle);
+
+  // Match by value sloppy
   if (!found.length) found = options.filter(opt => opt[0].match(re));
 
+  // Append other options by text, that has not already been added
   for (let i = 0; i < options.length; i++) {
     const opt = options[i];
     if (opt.length > 1 && found.indexOf(opt) != -1 && opt[1].match(re)) {
-      if (options.map(o => o[0]).indexOf(opt[0]) == -1) options.push(opt);
+      if (options.map(o => o[0]).indexOf(opt[0]) == -1) found.push(opt);
     }
   }
 
-  return options;
+  activeIndex = 0;
+  visibleOptions = found;
 }
 
 function keydown(e) {
@@ -64,11 +72,7 @@ function keydown(e) {
     return;
   }
 
-  if (!open) {
-    open = true;
-    return;
-  }
-
+  if (!open) return toggle(e);
   if (e.key == 'ArrowDown') activeIndex++;
   if (e.key == 'ArrowUp') activeIndex--;
   if (e.key == 'Enter') selectOption(e);
@@ -83,14 +87,14 @@ function keyup(e) {
 function selectOption(e) {
   let opt;
   if (e.type == 'keydown') {
-    opt = visibleOptions.length ? visibleOptions[activeIndex] : [];
+    opt = visibleOptions.length && visibleOptions[activeIndex] || options.filter(o => o[0] == value)[0];
   }
   else {
-    const needle = e.target.href.replace(/^.*?#\d+:/, '');
+    const needle = closestEl(e.target, '.select-field__option').href.replace(/^.*?#\d+:/, '');
     opt = visibleOptions.filter(opt => opt[0] == needle)[0] || [];
   }
 
-  if (!opt.length) opt = [''];
+  if (!opt || !opt.length) opt = [''];
   value = opt[0];
   open = false;
 }
@@ -103,25 +107,24 @@ async function renderHuman(needle) {
 }
 
 function toggle(e) {
-  const optionsEl = e && e.target && closestEl(e.target, '.select-field__options');
-  if (!optionsEl) open = !open;
+  const optionEl = e && e.target && closestEl(e.target, '.select-field__option');
+
+  if (!open) {
+    activeIndex = visibleOptions.map(o => o[0]).indexOf(value);
+    humanEl.setSelectionRange(0, 9999);
+  }
+
+  return optionEl ? selectOption(e) : (open = !open);
 }
 </script>
 
-<div class="select-field text-field" class:is-open="{open}" hidden="{hidden}" bind:this="{wrapperEl}" on:click|preventDefault="{toggle}">
+<div class="select-field text-field" class:is-open="{open}" hidden="{hidden}" bind:this="{wrapperEl}">
   <label for="{id}"><slot name="label">Label</slot></label>
   <input type="hidden" {name} bind:this="{hiddenEl}" bind:value on:keydown="{keydown}"/>
-  <input type="text" {placeholder} {id} {readonly} autocomplete="off"
-    bind:this="{humanEl}"
-    on:keydown="{keydown}"
-    on:keyup="{keyup}"/>
-  <div class="select-field__options">
+  <input type="text" {placeholder} {id} {readonly} autocomplete="off" bind:this="{humanEl}" on:keydown="{keydown}" on:keyup="{keyup}" on:click|preventDefault="{toggle}">
+  <div class="select-field__options" on:click|preventDefault="{toggle}">
     {#each visibleOptions as opt, i}
-      <a href="#{i}:{opt[0]}"
-        class:is-active="{i == activeIndex}"
-        on:click="{selectOption}"
-        on:mouseover="{() => { activeIndex = i }}"
-        tabindex="-1">{opt.length > 1 ? opt[1] : opt[0]}</a>
+      <a href="#{i}:{opt[0]}" class="select-field__option" class:is-active="{i == activeIndex}" on:mouseover="{() => { activeIndex = i }}" tabindex="-1">{opt.length > 1 ? opt[1] : opt[0]}</a>
     {/each}
   </div>
 </div>
