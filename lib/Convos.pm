6d57c09515b888807a005471ffd21b22b2edc295
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-06-18 08:50:28 +0900

    Released 4.20

--- lib/Convos.pm
+++ lib/Convos.pm
@@ -9,7 +9,7 @@ use Mojo::JSON qw(false true);
 use Mojo::Util;
 use Scalar::Util 'blessed';
 
-our $VERSION = '4.19';
+our $VERSION = '4.20';
 
 $ENV{CONVOS_REVERSE_PROXY} //= $ENV{MOJO_REVERSE_PROXY} || 0;
 
@@ -190,7 +190,7 @@ Convos - Multiuser chat application
 
 =head1 VERSION
 
-4.19
+4.20
 
 =head1 DESCRIPTION
 
