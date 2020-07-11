ee064b3148a1a6ca31e66edb4f9f7a3605c91d7a
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-07-11 22:44:41 +0900

    Fix rendering links inside `code`, closes #503

---
+++ __tests__/md.js
@@ -0,0 +1,42 @@
+import '../public/images/emojis.js';
+import {md} from '../assets/js/md';
+
+test('unchanged', () => {
+  expect(md('foo')).toBe('foo');
+  expect(md('Some text!')).toBe('Some text!');
+});
+
+test('blockquote', () => {
+  expect(md('> Some quote')).toBe('<blockquote>Some quote</blockquote>');
+});
+
+test('emoji', () => {
+  expect(countEmojis(':) :/ :( ;D &lt;3 :D :P ;) :heart:')).toBe(8);
+});
+
+test('em, strong', () => {
+  expect(md('Hey *foo* **bar** ***baz***!'))
+    .toBe('Hey <em>foo</em> <strong>bar</strong> <em><strong>baz</strong></em>!');
+});
+
+test('markdown link', () => {
+  expect(md('some [cool chat](https://convos.chat)'))
+    .toBe('some <a href="https://convos.chat" target="_blank">cool chat</a>');
+  expect(md('A link to https://convos.chat'))
+    .toBe('A link to <a href="https://convos.chat" target="_blank">https://convos.chat</a>');
+  expect(md('A link to mailto:jhthorsen@cpan.org'))
+    .toBe('A link to <a href="mailto:jhthorsen@cpan.org" target="_blank">jhthorsen@cpan.org</a>');
+});
+
+test('code', () => {
+  expect(md('is this \\`not code`, or..?'))
+    .toBe('is this `not code`, or..?');
+  expect(md('is this `not code`, or..?'))
+    .toBe('is this <code>not code</code>, or..?');
+  expect(md('not a `https://link.com`'))
+    .toBe('not a <code><a href="https://link.com" target="_blank">https://link.com</a></code>');
+});
+
+function countEmojis(str) {
+  return md(str).match(/class="emoji"/g).length;
+}
--- assets/js/md.js
+++ assets/js/md.js
@@ -5,19 +5,15 @@
  * @exports emojiAliases
  * @exports emojis
  * @exports md
- * @exports xmlEscape
  */
 
 import twemoji from 'twemoji';
 import {route} from '../store/Route';
 import {regexpEscape} from './util';
 
-const codeToHtmlRe = new RegExp('(\\\\?)`([^`]+)`', 'g');
 const emojiByGroup = {};
 const emojiByName = {};
 const linkRe = new RegExp('\\b[a-z]{2,5}://\\S+', 'g');
-const mdLinkRe = new RegExp('\\[([^\\]]+)\\]\\(([^)]+)\\)', 'g');
-const mdToHtmlRe = new RegExp('(^|\\s)(\\\\?)(\\*+)(\\w[^<]*?)\\3', 'g');
 
 // Modifying this from the outside will break emojiRe below
 export const emojiAliases = {
@@ -32,14 +28,6 @@ export const emojiAliases = {
 };
 
 const emojiRe = new RegExp('(^|\\s)(:\\w+:|' + Object.keys(emojiAliases).map(k => regexpEscape(emojiAliases[k])).join('|') + ')(?=\\W|$)', 'gi'); // :short_code:, :(, :), :/, :D, :P, ;), ;D, <3
-
-const xmlEscapeMap = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  "'": '&apos;',
-  '"': '&quot;',
-};
 
 /**
  * emojis() is a function to return either a single emoji or a list of emojis
@@ -86,8 +74,8 @@ export function emojis(lookup, type = 'single') {
  * md() can convert a (subset) of markdown rules into a HTML string.
  *
  * @example
- * // Hey <em>foo</em> <em>foo</em> <strong>bar</strong> <strong>bar</strong> <em><strong>baz</strong></em> <em><strong>baz</strong></em>
- * md("Hey *foo* _foo_ **bar** __baz__ ***baz***!");
+ * // Hey <em>foo</em> <strong>bar</strong> <em><strong>baz</strong></em> <em><strong>baz</strong></em>
+ * md("Hey *foo* **bar** ***baz***!");
  *
  * // A <a href="https://convos.chat">link</a>
  * md("A [link](https://convos.chat)");
@@ -108,39 +96,68 @@ export function emojis(lookup, type = 'single') {
  * @return {String} A string that might contain HTML tags.
  */
 export function md(str) {
-  str = str.replace(/[&<>"']/g, m => xmlEscapeMap[m]);
+  const state = {};
 
-  let mdLinks = 0;
-  str = str.replace(/^&gt;\s(.*)/, (all, quote) => {
-    return '<blockquote>' + quote + '</blockquote>';
-  }).replace(mdToHtmlRe, (all, b, esc, md, text) => {
+  [
+    xmlEscape,
+    mdLink,
+    mdUrl,
+    mdCode,
+    mdEmStrong,
+    mdEmojis,
+    mdBlockQuote,
+  ].forEach(fn => { str = fn(str, state) });
+
+  return str;
+}
+
+function mdBlockQuote(str) {
+  return str.replace(/^&gt;\s(.*)/, (all, quote) => '<blockquote>' + quote + '</blockquote>');
+}
+
+function mdCode(str) {
+  return str.replace(/(\\?)`([^`]+)`/g, (all, esc, text) => {
+    return esc ? all.replace(/^\\/, '') : '<code>' + text + '</code>';
+  });
+}
+
+function mdEmojis(str) {
+  return twemoji.parse(str.replace(emojiRe, (all, pre, emoji) => pre + (emojis(emoji).emoji || emoji)));
+}
+
+function mdEmStrong(str) {
+  return str.replace(/(^|\s)(\\?)(\*+)(\w[^<]*?)\3/g, (all, b, esc, md, text) => {
     if (md.length == 1) return esc ? all.replace(/^\\/, '') : b + '<em>' + text + '</em>';
     if (md.length == 2) return esc ? all.replace(/^\\/, '') : b + '<strong>' + text + '</strong>';
     if (md.length == 3) return esc ? all.replace(/^\\/, '') : b + '<em><strong>' + text + '</strong></em>';
     return all;
-  }).replace(mdLinkRe, (all, text, href) => {
-    mdLinks++;
+  });
+}
+
+function mdLink(str, state = {}) {
+  return str.replace(/\[([^\]]+)\]\(([^)]+)\)/g, (all, text, href) => {
     const scheme = href.match(/^\s*(\w+):/) || ['', ''];
     if (scheme[1] && ['http', 'https', 'mailto'].indexOf(scheme[1]) == -1) return all; // Avoid XSS links
-
+    state.mdLinks = true;
     const first = href.substring(0, 1);
     const target = ['/', '#'].indexOf(first) != -1 ? '' : ' target="_blank"';
     return '<a href="' + route.urlFor(href) + '"' + target + '>' + text + '</a>';
-  }).replace(codeToHtmlRe, (all, esc, text) => {
-    return esc ? all.replace(/^\\/, '') : '<code>' + text + '</code>';
   });
+}
 
-  if (!mdLinks) {
-    str = str.replace(linkRe, url => {
-      const parts = url.match(/^(.*?)(&\w+;|\W)?$/);
-      return '<a href="' + parts[1] + '" target="_blank">' + parts[1].replace(/^https:\/\//, '') + '</a>' + (parts[2] || '');
-    }).replace(/mailto:(\S+)/, (all, email) => {
-      if (all.indexOf('">') != -1) return all;
-      return '<a href="' + all + '" target="_blank">' + email + '</a>';
-    });
-  }
+function mdUrl(str, state = {}) {
+  if (state.mdLinks) return str;
 
-  str = twemoji.parse(str.replace(emojiRe, (all, pre, emoji) => pre + (emojis(emoji).emoji || emoji)));
+  return str.replace(linkRe, url => {
+    const parts = url.match(/^(.*?)(&\w+;|\W)?$/);
+    return '<a href="' + parts[1] + '" target="_blank">' + parts[1] + '</a>' + (parts[2] || '');
+  }).replace(/mailto:(\S+)/, (all, email) => {
+    if (all.indexOf('">') != -1) return all;
+    return '<a href="' + all + '" target="_blank">' + email + '</a>';
+  });
+}
 
-  return str;
+function xmlEscape(str) {
+  const map = {'&': '&amp;', '<': '&lt;', '>': '&gt;', "'": '&apos;', '"': '&quot;'};
+  return str.replace(/[&<>"']/g, (m) => map[m]);
 }
