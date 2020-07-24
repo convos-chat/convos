e841e1f0d1ba79c0232f65069f3e6580dbad2edf
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-07-24 10:22:37 +0900

    Add Convos::Plugin::Bot::Action::Spool

---
+++ lib/Convos/Plugin/Bot/Action/Spool.pm
@@ -0,0 +1,83 @@
+package Convos::Plugin::Bot::Action::Spool;
+use Mojo::Base 'Convos::Plugin::Bot::Action';
+
+use Mojo::File 'path';
+use YAML::XS ();
+
+has description => 'Send messages from spool directory on server.';
+has _dir        => undef;
+
+sub register {
+  my ($self, $bot, $config) = @_;
+  my $user = $bot->user;
+
+  $self->_dir(path($ENV{CONVOS_BOT_SPOOL_DIR} || $user->core->home->child($user->email, 'spool')));
+  $self->_dir->make_path unless -d $self->_dir;
+
+  Scalar::Util::weaken($self);
+  Mojo::IOLoop->recurring(
+    ($ENV{CONVOS_BOT_SPOOL_INTERVAL} || 1) => sub { $self and $self->_check_for_message($user); });
+}
+
+sub _check_for_message {
+  my ($self, $user) = @_;
+
+  my $files = $self->_dir->list->sort;
+  my $guard = 50;
+  while (my $file = pop @$files) {
+    my $message    = YAML::XS::Load($file->slurp);
+    my $connection = $user->get_connection($message->{connection_id});
+    next unless $connection and $connection->state eq 'connected';
+    $connection->send_p(@$message{qw(dialog_id message)})->catch(sub {
+      $user->core->log->warn(sprintf 'Bot send "%s" to "%s/%s": %s',
+        @$message{qw(message connection_id dialog_id)}, pop);
+    });
+    unlink $file;
+    return unless --$guard;    # Make sure we do not block for too long
+  }
+}
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+Convos::Plugin::Bot::Action::Spool - Send messages found in a spool directory
+
+=head1 DESCRIPTION
+
+This action will send messages found in a spool directory. Default directory is:
+
+  $CONVOS_HOME/your_bot@example.com/spool
+
+The files are sorted by name and not by date. The reason for this is that you
+might want to prioritize certain messages.
+
+=head2 File format
+
+The file must be a valid YAML file. Example message file:
+
+  ---
+  connection_id: irc-freenode
+  dialog_id: "#convos"
+  message: Some message
+
+=head1 ATTRIBUTES
+
+=head2 description
+
+See L<Convos::Plugin::Bot::Action/description>.
+
+=head1 METHODS
+
+=head2 register
+
+Sets up an recurring timer that checks for new messages. By default the bot can only
+send one message per second.
+
+=head1 SEE ALSO
+
+L<Convos::Plugin::Bot>, L<Convos>.
+
+=cut
---
+++ t/plugin-bot-spool.t
@@ -0,0 +1,34 @@
+use lib '.';
+use t::Helper;
+use t::Server::Irc;
+use Convos::Plugin::Bot::Action::Spool;
+
+plan skip_all => 'TEST_BOT=1' unless $ENV{TEST_BOT};
+
+$ENV{CONVOS_BOT_ALLOW_STANDALONE} = 1;
+$ENV{CONVOS_BOT_SPOOL_INTERVAL}   = 0.01;
+$ENV{CONVOS_BOT_EMAIL} ||= 'bot@convos.chat';
+
+my $t      = t::Helper->t;
+my $server = t::Server::Irc->new->start;
+
+my $bot   = $t->app->bot;
+my $spool = Convos::Plugin::Bot::Action::Spool->new;
+$spool->register($bot, {});
+$spool->_dir->child('whatever.yml')->spurt(test_file());
+
+my $connection = $bot->user->connection({protocol => 'irc', name => 'localhost'});
+my $event;
+$server->client($connection)->server_event_ok('_irc_event_privmsg', sub { $event = pop })
+  ->process_ok;
+is $event->{raw_line}, ':bot PRIVMSG #convos :too cool!', 'sent message';
+
+done_testing;
+
+sub test_file {
+  return <<'HERE';
+connection_id: irc-localhost
+dialog_id: "#convos"
+message: too cool!
+HERE
+}
