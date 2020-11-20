c4af49a7ad3b39f2a40822172e23784bb5726a6a
Jan Henning Thorsen <jhthorsen@cpan.org>
2020-11-21 08:51:45 +0900

    Fix jumping to bottom of conversation on initial render

--- assets/components/InfinityScroll.svelte
+++ assets/components/InfinityScroll.svelte
@@ -2,17 +2,19 @@
 import {createEventDispatcher} from 'svelte';
 import {findVisibleElements, isType} from '../js/util';
 
+const DEBUG = false;
 const dispatch = createEventDispatcher();
 const state = {scrollDelay: 80, scrollOffset: 80, scrollTop: 0};
 
+let cancelNextScroll = true;
 let className = '';
-let isManuallyScrolled = true;
 let scrollHeight = 0;
 
 export {className as class};
 export let pos = 'top';
 
 $: classNames = [className || 'infinity-scroll'].concat(['has-pos-' + pos]);
+$: DEBUG && console.log(location.href + ': ' + scrollHeight);
 
 function calculateDetails(infinityEl) {
   pos = infinityEl.scrollTop > scrollHeight - infinityEl.offsetHeight - state.scrollOffset ? 'bottom'
@@ -34,12 +36,21 @@ function onReady(infinityEl, params) {
 
 function onScroll(infinityEl) {
   if (state.scrollTid) clearTimeout(state.scrollTid);
-  if (Math.abs(state.scrollTop - state.infinityEl.scrollTop) > 600) onScrolled(infinityEl); // 600 is a more or less randomly picked number
   state.scrollTid = setTimeout(() => onScrolled(infinityEl), state.scrollDelay);
 }
 
 function onScrolled(infinityEl) {
-  if (isManuallyScrolled) return (isManuallyScrolled = false);
+  if (DEBUG) console.log('onScrolled pos=' + state.scrollTop + '/' + infinityEl.scrollTop + ' cancelNextScroll=' + cancelNextScroll);
+
+  if (cancelNextScroll) {
+    // onScrolled() can will get triggered when scrollTo() changes infinityEl.scrollTop.
+    // Need to cancel the next "scroll" event as well, in case "scrollTop" was not set correctly.
+    cancelNextScroll = Math.abs(infinityEl.scrollTop - state.scrollTop) > 40;
+    if (cancelNextScroll) infinityEl.scrollTop = state.scrollTop;
+    if (state.scrollTid) clearTimeout(state.scrollTid);
+    return;
+  }
+
   state.scrollTop = infinityEl.scrollTop;
   calculateDetails(infinityEl);
   dispatch('scrolled', state);
@@ -54,12 +65,15 @@ function onUpdate(infinityEl, {scrollHeight}) {
 
 function scrollTo(pos) {
   if (isType(pos, 'undef')) return false;
-  if (pos == -1) pos = scrollHeight - pos;
+  if (pos == -1) pos = scrollHeight;
   if (isType(pos, 'string')) pos = state.infinityEl.querySelector(pos);
   if (pos && pos.tagName) pos = pos.offsetTop;
   if (isType(pos, 'undef')) return false;
-  isManuallyScrolled = true;
+  if (pos < 0) return false;
+  cancelNextScroll = true;
   state.infinityEl.scrollTop = pos;
+  state.scrollTop = state.infinityEl.scrollTop;
+  if (DEBUG) console.log('scrollTo pos=' + pos + '/' + state.scrollTop);
   return true;
 }
 </script>
