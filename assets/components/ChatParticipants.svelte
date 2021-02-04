6da3e4540e076bc27928e984b1c00583680d2c3b
Jan Henning Thorsen <jhthorsen@cpan.org>
2021-02-04 11:53:15 +0900

    Fix number of participants in ChatParticipants

--- assets/components/ChatParticipants.svelte
+++ assets/components/ChatParticipants.svelte
@@ -17,7 +17,7 @@ function joinConversation(e, participant) {
 {#if $participants.length}
   <div class="sidebar-right">
     <nav class="sidebar-right__nav">
-      <h3>{$l('Participants (%1)', participants.length)}</h3>
+      <h3>{$l('Participants (%1)', $participants.length)}</h3>
       {#each $participants.toArray() as participant}
         <Link href="/chat/{conversation.connection_id}/{participant.id}" on:click="{e => joinConversation(e, participant)}" class="participant {modeClassNames(participant.modes)}">
           <Icon name="pick:{participant.id}" family="solid" color="{participant.color}"/>
