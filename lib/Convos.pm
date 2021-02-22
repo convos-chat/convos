73b0c9cb5f06f787b7882a01a2241542da9d3b01
Jan Henning Thorsen <jhthorsen@cpan.org>
2021-02-22 09:18:01 +0900

    Released v5.32

--- lib/Convos.pm
+++ lib/Convos.pm
@@ -11,7 +11,7 @@ use Scalar::Util 'blessed';
 
 use constant CONVOS_GET => +($ENV{CONVOS_COMMAND} || '') eq 'get';
 
-our $VERSION = '5.31';
+our $VERSION = '5.32';
 
 $ENV{CONVOS_REVERSE_PROXY} //= $ENV{MOJO_REVERSE_PROXY} || 0;
 
@@ -203,7 +203,7 @@ Convos - Multiuser chat application
 
 =head1 VERSION
 
-5.31
+5.32
 
 =head1 DESCRIPTION
 
