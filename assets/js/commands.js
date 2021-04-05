fb08f8cc4c5f9d41ff97e158df9312336f062342
PeGaSuS <25697531+PeGaSuS-Coder@users.noreply.github.com>
2021-04-05 04:09:09 +0200

    Added support for some other services such as MemoServ, BotServ, HostServ and OperServ in commands.js (#580)
    - Added support for some other services such as MemoServ, BotServ, HostServ and OperServ
    - Changed the rewrite from "/msg target" to "/quote target" which makes Convos aliases work out of the box on DALnet which forces the users to address themselves to services with "/msg NickServ@services.dal.net", e.g. Using "/quote" instead "/msg" solves that issue.

--- assets/js/commands.js
+++ assets/js/commands.js
@@ -43,16 +43,24 @@ add('/names', '/names', 'Show participants in the channel.');
 add('/invite', '/invite <nick> [#channel]', 'Invite a user to a channel.');
 add('/reconnect', '/reconnect', 'Restart the current connection.');
 add('/clear', '/clear history <#channel> or /clear history <nick>', 'Delete all history for the given conversation.');
-add('/oper', '/oper <msg>', 'Send server operator messages.');
+add('/oper', '/oper <nick> <password>', 'Gain operator status on a network.');
 add('/cs', '/cs <msg>', 'Send a message to chanserv.');
 add('/ns', '/ns <msg>', 'Send a message to nickserv.');
+add('/ms', '/ms <msg>', 'Send a message to memoserv.');
+add('/hs', '/hs <msg>', 'Send a message to hostserv.');
+add('/bs', '/bs <msg>', 'Send a message to botserv.');
+add('/os', '/os <msg>', 'Send a message to operserv.');
 add('/quote', '/quote <irc-command>', 'Allow you to send any raw IRC message.');
 
 const rewrite = (from, to) => (rewriteRule[from] = to);
 
 rewrite('/close', '/part');
 rewrite('/shrug', (parts) => '/say ' + parts.concat('¯\\_(ツ)_/¯').join(' '));
-rewrite('/cs', '/msg chanserv');
+rewrite('/cs', '/quote chanserv');
+rewrite('/ns', '/quote nickserv');
+rewrite('/ms', '/quote memoserv');
+rewrite('/hs', '/quote hotserv');
+rewrite('/bs', '/quote botserv');
+rewrite('/os', '/quote operserv');
 rewrite('/j', '/join');
-rewrite('/ns', '/msg nickserv');
 rewrite('/raw', '/quote');
