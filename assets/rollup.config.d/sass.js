c93524a440afcf2cab88064645dfb334b791ec74
Jan Henning Thorsen <jhthorsen@cpan.org>
2021-07-18 10:45:11 +0900

    Fix Unsupported output style "nested"

--- assets/rollup.config.d/sass.js
+++ assets/rollup.config.d/sass.js
@@ -2,6 +2,6 @@ const sass = require('sass');
 const scss = require('rollup-plugin-scss');
 
 module.exports = function(config, {isDev}) {
-  const outputStyle = isDev ? 'nested' : 'compressed';
+  const outputStyle = isDev ? null : 'compressed';
   config.plugins.push(scss({outputStyle, sass, sourceMap: true, watch: './assets/sass'}));
 };
