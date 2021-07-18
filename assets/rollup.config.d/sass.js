72d51d0072b2d124d4f3448832e6930b7198a631
Jan Henning Thorsen <jhthorsen@cpan.org>
2021-07-18 10:12:52 +0900

    Production css files gets minified

--- assets/rollup.config.d/sass.js
+++ assets/rollup.config.d/sass.js
@@ -1,6 +1,7 @@
 const sass = require('sass');
 const scss = require('rollup-plugin-scss');
 
-module.exports = function(config) {
-  config.plugins.push(scss({sass, sourceMap: true, watch: './assets/sass'}));
+module.exports = function(config, {isDev}) {
+  const outputStyle = isDev ? 'nested' : 'compressed';
+  config.plugins.push(scss({outputStyle, sass, sourceMap: true, watch: './assets/sass'}));
 };
