a7882444adf764955752e699e40b120e0f093b44
Jan Henning Thorsen <jhthorsen@cpan.org>
2021-09-13 07:26:30 +0200

    Add "Retry" button to loading screen

--- assets/page/Fallback.svelte
+++ assets/page/Fallback.svelte
@@ -1,15 +1,17 @@
 <script>
 import Icon from '../components/Icon.svelte';
+import {fade} from 'svelte/transition';
 import {getContext} from 'svelte';
 import {l, lmd} from '../store/I18N';
 import {route} from '../store/Route';
-import {settings} from '../js/util';
+import {settings, timer} from '../js/util';
 
 export let title = 'Loading...';
 
 const messages = {'loading': 'Loading...', 'not_found': 'Not Found'};
 const socket = getContext('socket');
 const user = getContext('user');
+const timeout = timer(5000);
 
 $: status = calculateStatus($route, $user);
 $: startHref = route.urlFor(user.email ? '/chat' : '/login');
@@ -31,7 +33,14 @@ function calculateStatus($route, user) {
     {:else}
       <p><Icon name="spinner fa-spin"/> {$l('Loading user data...')}</p>
     {/if}
-    <p><a class="btn" href="{settings('contact')}"><Icon name="paper-plane"/> {$l('Contact admin')}</a></p>
+    <p>
+      <a class="btn" href="{settings('contact')}"><Icon name="paper-plane"/> {$l('Contact admin')}</a>
+      {#await timeout}
+        <!-- Go to start page -->
+      {:then}
+        <a href="{startHref}" target="_self" class="btn is-secondary" transition:fade><Icon name="redo"/> {$l('Retry')}</a>
+      {/await}
+    </p>
   {:else if status == 'not_found'}
     <p>{$l('The Convos Team have been searching and searching, but the requested page could not be found.')}</p>
     <p><a href="{startHref}" target="_self" class="btn"><Icon name="play"/> {$l('Go to start page')}</a></p>
