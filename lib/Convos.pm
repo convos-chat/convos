8eb4e983619793998ce32696d68998c84a9dc563
Jan Henning Thorsen <jan.henning@thorsen.pm>
2022-09-20 07:44:12 +0900

    Released v7.06

--- lib/Convos.pm
+++ lib/Convos.pm
@@ -2,18 +2,18 @@ package Convos;
 use Mojo::Base 'Mojolicious';
 
 use Carp qw(croak);
-use Cwd ();
+use Cwd  ();
 use Convos::Core;
-use Convos::Util qw(is_true require_module);
+use Convos::Util  qw(is_true require_module);
 use File::HomeDir ();
-use Mojo::File qw(path);
-use Mojo::JSON qw(false true);
+use Mojo::File    qw(path);
+use Mojo::JSON    qw(false true);
 use Mojo::Util;
 use Scalar::Util qw(blessed);
 
 use constant CONVOS_GET => +($ENV{CONVOS_COMMAND} || '') eq 'get';
 
-our $VERSION = '7.05';
+our $VERSION = '7.06';
 
 $ENV{CONVOS_REVERSE_PROXY} //= is_true 'ENV:MOJO_REVERSE_PROXY';
 $ENV{MOJO_REVERSE_PROXY}   //= is_true 'ENV:CONVOS_REVERSE_PROXY';
@@ -243,7 +243,7 @@ Convos - Multiuser chat application
 
 =head1 VERSION
 
-7.05
+7.06
 
 =head1 DESCRIPTION
 
